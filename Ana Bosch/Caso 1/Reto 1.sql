2e99bac700bf506801d93b9d052a20d7dae95c1f
AlexArcoCivica <156660711+AlexArcoCivica@users.noreply.github.com>
2024-04-15 19:48:03 +0200

    Comentario Reto 1.sql

--- Ana Bosch/Caso 1/Reto 1.sql
+++ Ana Bosch/Caso 1/Reto 1.sql
@@ -7,3 +7,16 @@ right join case01.customers customers
 left join case01.menu menu
 	on sales.product_id = menu.product_id
 group by customers.customer_id;
+
+
+/*
+Comentarios Alex.
+ 
+Resultado: Correcto
+Código: Correcto
+Legibilidad: Correcto
+
+Todo perfecto, limpiando la salida para quitar los nulos y mostrando todos los clientes aunque no hicieran compra, además del uso de alias para las tablas.
+Empezamos con un reto muy fácil pero ya se complicará jeje. Estoy seguro de que voy a aprender mucho más yo de ti que tu de mí y tengo ganas de ver los próximos retos.
+A por el siguiente! :)
+*/
