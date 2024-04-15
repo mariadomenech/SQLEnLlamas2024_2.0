84c107199a677bb7e2d991a0879843809fe70cd1
Joram <166694672+joramarcoi@users.noreply.github.com>
2024-04-15 09:57:15 +0200

    Create ejercicio 1_1.sql

---
+++ Joram Arco/Case 1/ejercicio 1_1.sql
@@ -0,0 +1,18 @@
+select
+	customers.customer_id as cliente,
+	isnull(sum(menu.price), 0) as importe_total
+
+from
+	[case01].[sales] sales
+join
+	[case01].[menu] menu
+on
+	(sales.product_id = menu.product_id)
+right join 
+	[case01].[customers] customers
+on
+	(sales.customer_id = customers.customer_id)
+group by
+	customers.customer_id
+order by
+	customers.customer_id asc
