1a39811fd821b5230006b9728b15e2b9170756e2
Jcroma92 <166692958+Jcroma92@users.noreply.github.com>
2024-04-15 12:07:32 +0200

    Case01.sql

--- Carlos Rodriguez/Week 1/Case01.sql
+++ Carlos Rodriguez/Week 1/Case01.sql
@@ -1,5 +1,5 @@
-SELECT c.customer_id, COALESCE(SUM(m.price), 0) as Total_Gastado
-FROM case01.customers c
-LEFT JOIN case01.sales s ON c.customer_id = s.customer_id
-LEFT JOIN case01.menu m ON s.product_id = m.product_id
-GROUP BY c.customer_id;
+SELECT customers.customer_id, COALESCE(SUM(menu.price), 0) as Total_Gastado
+FROM case01.customers customers
+LEFT JOIN case01.sales sales ON customers.customer_id = sales.customer_id
+LEFT JOIN case01.menu menu ON sales.product_id = menu.product_id
+GROUP BY customers.customer_id;
