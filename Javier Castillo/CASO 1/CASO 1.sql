326a0e1e1d60213f7070af0230e78c8f10c3f61a
jcastillo-civica <61198075+jcastillo-civica@users.noreply.github.com>
2024-04-15 23:18:21 +0200

    CASO 1

---
+++ Javier Castillo/CASO 1/CASO 1.sql
@@ -0,0 +1,10 @@
+/****** Gasto total de cada cliente  ******/
+SELECT 
+  C.customer_id AS CLIENTE
+, COALESCE(SUM(M.price),0) AS GASTO_TOTAL
+FROM [SQL_EN_LLAMAS_ALUMNOS].[case01].[customers] AS C
+LEFT OUTER JOIN [SQL_EN_LLAMAS_ALUMNOS].[case01].[sales] AS S
+	ON C.customer_id = S.customer_id
+LEFT OUTER JOIN [SQL_EN_LLAMAS_ALUMNOS].[case01].[menu] AS M
+	ON S.product_id = M.product_id
+GROUP BY C.customer_id;
