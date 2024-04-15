3c2e7b1601caee4ee9ffccae8dd8a3ba9cdca6e4
peter-navarro <166490623+peter-navarro@users.noreply.github.com>
2024-04-15 19:56:37 +0200

    Update challenge_01.sql

--- Pedro Navarro/Case 01/challenge_01.sql
+++ Pedro Navarro/Case 01/challenge_01.sql
@@ -5,9 +5,9 @@ SELECT
 	ISNULL(SUM(m.price), 0) AS total_spent
 FROM SQL_EN_LLAMAS_ALUMNOS.case01.customers AS c
 LEFT JOIN SQL_EN_LLAMAS_ALUMNOS.case01.sales AS s
-ON c.customer_id = s.customer_id
+    ON c.customer_id = s.customer_id
 LEFT JOIN SQL_EN_LLAMAS_ALUMNOS.case01.menu AS m
-ON s.product_id = m.product_id
+    ON s.product_id = m.product_id
 GROUP BY c.customer_id;
 
 
@@ -19,7 +19,7 @@ GROUP BY c.customer_id;
 			SUM(price) AS total_spent
 		FROM SQL_EN_LLAMAS_ALUMNOS.case01.sales AS s
 		LEFT JOIN SQL_EN_LLAMAS_ALUMNOS.case01.menu AS m
-		ON s.product_id = m.product_id
+		    ON s.product_id = m.product_id
 		GROUP BY customer_id
 	),
 	
@@ -29,7 +29,7 @@ GROUP BY c.customer_id;
 			ISNULL(s.total_spent, 0) AS total_spent
 		FROM SQL_EN_LLAMAS_ALUMNOS.case01.customers AS c
 		LEFT JOIN TOTAL_SPENT_PER_CUSTOMER AS s
-		ON c.customer_id = s.customer_id
+		    ON c.customer_id = s.customer_id
 	)
 
 SELECT *
