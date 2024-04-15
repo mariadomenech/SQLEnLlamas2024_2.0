714740d59665fd4057759725f4be80f4df0b5699
juanpedronavio <156901383+juanpedronavio@users.noreply.github.com>
2024-04-15 18:52:28 +0200

    Solución Juanpe 1.1.sql
    Solución Juanpe 1.1.sql

--- Joaquín Caruncho/Ejercicio1_1.sql
+++ Joaquín Caruncho/Ejercicio1_1.sql
@@ -9,3 +9,16 @@ from
 		on b.product_id = c.product_id
 group by a.customer_id
 order by a.customer_id;
+
+/*
+Comentarios Juanpe.
+Todo correcto. 
+Resultado: OK
+Código: OK. Bien partiendo de la de customers para obtener a todos hayan hecho alguna compra o no.
+Legibilidad: OK
+Sencillito este primer ejercicio, pero bueno para ir marcando los puntos que queremos evaluar, a parte de la solución en sí del ejercicio, cosas a valorar que si has reflejado en tu propuesta:
+  - Bien por limpiar la salida para mostrar 0 y no ‘null’ en aquel que no ha hecho ninguna compra.
+  - Bien por dar nombres a las columnas.
+  - Bien por establecer un orden en la salida.
+Ahora mismo esto parecen cosas triviales, pero no todo el mundo cae en hacerlas y estos detalles están la diferencia.
+*/
