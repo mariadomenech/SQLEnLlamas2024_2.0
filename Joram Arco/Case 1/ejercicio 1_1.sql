2d222c0fb5e7e584f96bfcd2caef634aa2fee7a1
Joram <166694672+joramarcoi@users.noreply.github.com>
2024-04-15 09:58:44 +0200

    Update ejercicio 1_1.sql

--- Joram Arco/Case 1/ejercicio 1_1.sql
+++ Joram Arco/Case 1/ejercicio 1_1.sql
@@ -1,18 +1,17 @@
 select
 	customers.customer_id as cliente,
 	isnull(sum(menu.price), 0) as importe_total
-
 from
-	[case01].[sales] sales
+	case01.sales sales
 join
-	[case01].[menu] menu
+	case01.menu menu
 on
 	(sales.product_id = menu.product_id)
 right join 
-	[case01].[customers] customers
+	case01.customers customers
 on
 	(sales.customer_id = customers.customer_id)
 group by
 	customers.customer_id
 order by
-	customers.customer_id asc
+	customers.customer_id asc;
