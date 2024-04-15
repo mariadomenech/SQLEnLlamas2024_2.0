c6296fc3a02bd8ad3c5c82ce1d7865aab3e896d3
irenelacivica <156344357+irenelacivica@users.noreply.github.com>
2024-04-15 13:55:50 +0200

    Update Ejercicio 1.sql

--- Julia Rivera /Case 1/Ejercicio 1.sql
+++ Julia Rivera /Case 1/Ejercicio 1.sql
@@ -19,3 +19,18 @@ GROUP BY C.CUSTOMER_ID
 
 GO
 
+/*********************************************************/
+/***************** COMENTARIO IRENE **********************/
+/*********************************************************/
+/*
+
+El resultado es completamente correcto!
+
+Aun así te voy a dar dos tips:
+	- En vez de usar el case when, puedes usar directamente un COALESCE (SUM (price),0) AS TOTAL_GASTADO --> Con esto el código se ve más conciso.
+	- Yo primero me pararía a pensar qué tabla es la dimension principal y eligiría una dirección de join. 
+		+ Al coger la columna de CUSTOMER_ID de la tabla CUSTOMERS, esta es tu tabla principal. A partir de ésta, vamos encajando las otras tablas. Entonces, de la
+		tabla CUSTOMERS podemos ir a la de SALES con un LEFT JOIN y seguidamente a la de MENU con un LEFT JOIN. 
+		+ De esta forma ya tenemos todas las columnas ligadas en una sola dirección (LEFT) desde la tabla principal (CUSTOMERS).
+
+*/
