e2aa58879a6d1a8030c69cf6bcb66692fa679721
jcastillo-civica <61198075+jcastillo-civica@users.noreply.github.com>
2024-04-15 23:22:15 +0200

    Update CASO 1.sql

--- Javier Castillo/CASO 1/CASO 1.sql
+++ Javier Castillo/CASO 1/CASO 1.sql
@@ -7,4 +7,5 @@ LEFT OUTER JOIN [SQL_EN_LLAMAS_ALUMNOS].[case01].[sales] AS S
 	ON C.customer_id = S.customer_id
 LEFT OUTER JOIN [SQL_EN_LLAMAS_ALUMNOS].[case01].[menu] AS M
 	ON S.product_id = M.product_id
-GROUP BY C.customer_id;
+GROUP BY C.customer_id
+ORDER BY C.customer_id;
