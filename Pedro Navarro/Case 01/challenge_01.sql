b31701e018697b3b5594451bc5222654fa4b9c41
peter-navarro <166490623+peter-navarro@users.noreply.github.com>
2024-04-15 20:12:31 +0200

    Update challenge_01.sql

--- Pedro Navarro/Case 01/challenge_01.sql
+++ Pedro Navarro/Case 01/challenge_01.sql
@@ -1,8 +1,8 @@
 -- RETO 1. ¿CUÁNTO HA GASTADO EN TOTAL CADA CLIENTE EN EL RESTAURANTE?
 -- SOLUCIÓN 1: MÁS SENCILLA (UNA SOLA QUERY)
 SELECT
-	c.customer_id,
-	ISNULL(SUM(m.price), 0) AS total_spent
+	c.customer_id
+	,ISNULL(SUM(m.price), 0) AS total_spent
 FROM SQL_EN_LLAMAS_ALUMNOS.case01.customers AS c
 LEFT JOIN SQL_EN_LLAMAS_ALUMNOS.case01.sales AS s
     ON c.customer_id = s.customer_id
@@ -15,8 +15,8 @@ GROUP BY c.customer_id;
 ;WITH
 	TOTAL_SPENT_PER_CUSTOMER AS (
 		SELECT
-			customer_id,
-			SUM(price) AS total_spent
+			customer_id
+			,SUM(price) AS total_spent
 		FROM SQL_EN_LLAMAS_ALUMNOS.case01.sales AS s
 		LEFT JOIN SQL_EN_LLAMAS_ALUMNOS.case01.menu AS m
 		    ON s.product_id = m.product_id
@@ -25,8 +25,8 @@ GROUP BY c.customer_id;
 	
 	TOTAL_SPENT_PER_TOTAL_CUSTOMER AS (
 		SELECT
-			c.customer_id,
-			ISNULL(s.total_spent, 0) AS total_spent
+			c.customer_id
+			,ISNULL(s.total_spent, 0) AS total_spent
 		FROM SQL_EN_LLAMAS_ALUMNOS.case01.customers AS c
 		LEFT JOIN TOTAL_SPENT_PER_CUSTOMER AS s
 		    ON c.customer_id = s.customer_id
