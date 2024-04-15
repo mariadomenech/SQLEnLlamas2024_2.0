ac80fb7cf54e92b905786a22760e34a6d658004d
MiguelCasoLassaletta <166695557+MiguelCasoLassaletta@users.noreply.github.com>
2024-04-15 10:40:26 +0200

    Update Ejercicio 1.sql

--- Miguel Caso/Case 1/Ejercicio 1.sql
+++ Miguel Caso/Case 1/Ejercicio 1.sql
@@ -1,1 +1,7 @@
-
+SELECT
+    c.customer_id AS cliente,
+    COALESCE (SUM (price),0) AS total_gasto
+FROM SQL_EN_LLAMAS.CASE01.SALES a
+LEFT JOIN SQL_EN_LLAMAS.CASE01.MENU b ON a.product_id = b.product_id
+RIGHT JOIN SQL_EN_LLAMAS.CASE01.CUSTOMERS c ON a.customer_id = c.customer_id
+GROUP BY c.customer_id;
