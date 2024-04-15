e3808c509cf55ee166d5e41d217176241f396896
reguera94 <74610965+reguera94@users.noreply.github.com>
2024-04-15 17:24:51 +0200

    Update Ejercicio1.sql

--- Jose Reguera /Case 01/Ejercicio1.sql
+++ Jose Reguera /Case 01/Ejercicio1.sql
@@ -1,11 +1,11 @@
-select A.customer_id as NombreCliente ,
+select B.customer_id as NombreCliente ,
 	Isnull(sum(C.price),0) as Dinero_Gastado 
 
-from case01.customers A
+from case01.sales A
 
-  full outer join case01.sales B
+  full outer join case01.customers B
 	 on A.customer_id=B.customer_id
   full outer join case01.menu C
-	 on B.product_id=C.product_id
+	 on A.product_id=C.product_id
 
-	 group by A.customer_id;
+	 group by B.customer_id;
