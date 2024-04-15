ddf860af2cb4d70a2e8529deb4a815b4e7ef3770
irenelacivica <156344357+irenelacivica@users.noreply.github.com>
2024-04-15 13:57:51 +0200

    Corrección Irene Ejercicio 1.sql

--- Miguel Caso/Case 1/Ejercicio 1.sql
+++ Miguel Caso/Case 1/Ejercicio 1.sql
@@ -13,6 +13,12 @@ GROUP BY c.customer_id;
 
 /* 
 
-Perfecto Miguel!!! 
+El resultado es completamente correcto!!
+
+No obstante te voy a dar un tip:
+	- Yo primero me pararía a pensar qué tabla es la dimension principal y eligiría una dirección de join. 
+		+ Al coger la columna de CUSTOMER_ID de la tabla CUSTOMERS, esta es tu tabla principal. A partir de ésta, vamos encajando las otras tablas. Entonces, de la
+		tabla CUSTOMERS podemos ir a la de SALES con un LEFT JOIN y seguidamente a la de MENU con un LEFT JOIN. 
+		+ De esta forma ya tenemos todas las columnas ligadas en una sola dirección (LEFT) desde la tabla principal (CUSTOMERS).
 
 */
