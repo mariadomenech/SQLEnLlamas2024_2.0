50c40fd5153da9a2404eae9c04b89832adc6ffb3
Jose Sanchez Robles <josesanxez@gmail.com>
2024-04-15 11:32:28 +0200

    Update Ejercicio 1.sql

--- Jose Sanchez/Case 1/Ejercicio 1.sql
+++ Jose Sanchez/Case 1/Ejercicio 1.sql
@@ -1,1 +1,9 @@
-
+SELECT
+    customers.customer_id,
+    ISNULL (sum(menu.price), 0)
+FROM
+    case01.customers as customers
+    LEFT JOIN case01.sales as sales ON customers.customer_id = sales.customer_id
+    LEFT JOIN case01.menu as menu ON menu.product_id = sales.product_id
+GROUP BY
+    customers.customer_id;
