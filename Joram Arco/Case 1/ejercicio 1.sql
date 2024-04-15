9d528fe6b4ca7fe0a1e4d7f09dbf59df2e09dea4
juanpedronavio <156901383+juanpedronavio@users.noreply.github.com>
2024-04-15 18:52:55 +0200

    Solución Juanpe 1.1.sql
    Solución Juanpe 1.1.sql

--- Joram Arco/Case 1/ejercicio 1.sql
+++ Joram Arco/Case 1/ejercicio 1.sql
@@ -15,3 +15,16 @@ group by
 	customers.customer_id
 order by
 	customers.customer_id asc;
+
+/*
+Comentarios Juanpe.
+Todo correcto. 
+Resultado: OK
+Código: OK. Bien, buen uso del right para traer a todos hayan hecho alguna compra o no.
+Legibilidad: OK
+Sencillito este primer ejercicio, pero bueno para ir marcando los puntos que queremos evaluar, a parte de la solución en sí del ejercicio, cosas a valorar que si has reflejado en tu propuesta:
+  - Bien por limpiar la salida para mostrar 0 y no ‘null’ en aquel que no ha hecho ninguna compra.
+  - Bien por dar nombres a las columnas.
+  - Bien por establecer un orden en la salida.
+Ahora mismo esto parecen cosas triviales, pero no todo el mundo cae en hacerlas y estos detalles están la diferencia.
+*/
