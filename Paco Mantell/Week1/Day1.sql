838e1b6c57707eb6dc5ca1bec069f64367540fa3
pacomantell <109753909+pacomantell@users.noreply.github.com>
2024-04-15 09:40:24 +0200

    Create Day1.sql

---
+++ Paco Mantell/Week1/Day1.sql
@@ -0,0 +1,9 @@
+/** Total gastado por cada cliente**/
+SELECT A.customer_id,
+  ISNULL(SUM(C.price), 0) total
+FROM case01.customers A
+LEFT JOIN case01.sales B
+	ON A.customer_id = B.customer_id
+LEFT JOIN case01.menu C
+	ON B.product_id = C.product_id
+GROUP BY A.customer_id;
