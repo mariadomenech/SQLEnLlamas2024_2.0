d0e918509dae2d49d6766268b8b0b16c9ee94e67
irenelacivica <156344357+irenelacivica@users.noreply.github.com>
2024-04-16 14:47:38 +0200

    Corrección Irene Ejercicio 2.sql

--- Jose Morales/Case 1/Ejercicio 2.sql
+++ Jose Morales/Case 1/Ejercicio 2.sql
@@ -6,3 +6,12 @@ right join case01.customers cust
   on sales.customer_id = cust.customer_id
 group by cust.customer_id
 order by Num_visitas desc;
+
+/*********************************************************/
+/***************** COMENTARIO IRENE **********************/
+/*********************************************************/
+/*
+
+Perfecto Jose!!!
+
+*/
