4ea0e3e87e3b498a053130f5ed16842fb7267848
reguera94 <74610965+reguera94@users.noreply.github.com>
2024-04-16 21:21:04 +0200

    Create Ejercicio2.sql

---
+++ Jose Reguera /Case 01/Ejercicio2.sql
@@ -0,0 +1,7 @@
+select  a.customer_id as NombreCliente,
+		count(distinct b.order_date) as	DiasVisitados	
+from [case01].[customers] a
+	    left outer join [case01].[sales] b
+			on a.customer_id=b.customer_id
+group by a.customer_id
+order by 	DiasVisitados	desc
