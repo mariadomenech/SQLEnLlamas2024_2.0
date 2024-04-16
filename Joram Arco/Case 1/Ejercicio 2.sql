1c028e94e438fd82db1905a7df4b5f5ae995767d
Joram <166694672+joramarcoi@users.noreply.github.com>
2024-04-16 09:43:04 +0200

    Create Ejercicio 2.sql

---
+++ Joram Arco/Case 1/Ejercicio 2.sql
@@ -0,0 +1,13 @@
+SELECT 
+	customers.customer_id,
+	COALESCE(COUNT(distinct sales.order_date), 0) AS NUM_VISITAS
+FROM 
+	case01.customers customers
+LEFT JOIN 
+	case01.sales sales 
+	ON (customers.customer_id = sales.customer_id)
+GROUP BY
+	customers.customer_id
+ORDER BY
+	CUSTOMER_ID ASC;
+-- Según SQL Query Comparison Tool el orden sería así, pero vería más lógico mostrar el orden según el número de visitas :)
