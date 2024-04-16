deb95d333135b9e0a4a3af9be6dccc68d8893577
elisa-perez <166694066+elisa-perez@users.noreply.github.com>
2024-04-16 10:30:59 +0200

    Correccion Ejercicio1_1.sql

--- Elisa Pérez /Case 1/Ejercicio1_1.sql
+++ Elisa Pérez /Case 1/Ejercicio1_1.sql
@@ -21,3 +21,15 @@ Solo faltaría implementar al cliente que falta, ten en cuenta que también hay 
 Para tratar campos nulos puedes probar IFNULL(), NVL(), aunque en SQLServer (donde estamos haciendo los ejercicios) se usaría ISNULL() por ejemplo ya que Josep en este caso quiere ver un 0 y no NULL en el campo
 A mí personalmente me gusta poner los agregados y funciones en Mayusc. y los atributos en minusc, ya que mejora la legibilidad
 pero eso es gusto propio. Ánimo Elisa que seguro lo consigues!. Intenta plasmas la corrección bajo este comentario. Cualquier duda me tienes a tu disposición*/
+
+/********************** CORRECCION *********************/
+-- Muchas gracias por los comentarios! :)
+
+SELECT  C.customer_id,
+	ISNULL(SUM(M.price),0) AS total_price
+FROM case01.sales S
+JOIN case01.menu M
+	ON S.product_id = M.product_id
+RIGHT JOIN case01.customers C
+	ON S.customer_id = C.customer_id
+GROUP BY C.customer_id;
