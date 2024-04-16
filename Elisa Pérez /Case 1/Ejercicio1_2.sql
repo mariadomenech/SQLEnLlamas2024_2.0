8b591c9652b07db0e982da550dea830dfb155471
DanIngeneer <156660817+DaniNgeneer@users.noreply.github.com>
2024-04-16 11:16:48 +0200

    Update Ejercicio1_2.sql

--- Elisa Pérez /Case 1/Ejercicio1_2.sql
+++ Elisa Pérez /Case 1/Ejercicio1_2.sql
@@ -4,3 +4,11 @@ FROM case01.sales S
 RIGHT JOIN case01.customers C
   ON S.customer_id = C.customer_id
 GROUP BY C.customer_id;
+
+
+/***********************************************/
+/***************COMENTARIO DANI*****************/
+/***********************************************/
+/*Resultado correcto, buen manejo del DISTINCT para no contar varias veces el mismo día. Me gusta el 
+enfoque del RIGHT JOIN ya que se ve que has tenido en cuenta el origen y apuntado de las tablas,
+como detallín te recomiendo que las indentaciones las hagas con TAB. Enhorabuena Elisa, a darlo todo!*/
