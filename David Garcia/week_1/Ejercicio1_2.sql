73f90aba0324a3afad3df23950d2467fe7dbf1f1
beatrizexposito <156438327+beatrizexposito@users.noreply.github.com>
2024-04-16 17:41:51 +0200

    Comentario Ejercicio1_2.sql

--- David Garcia/week_1/Ejercicio1_2.sql
+++ David Garcia/week_1/Ejercicio1_2.sql
@@ -6,3 +6,27 @@ left join case01.sales s
 on c.customer_id=s.customer_id
 group by c.customer_id
 order by visitas desc;
+
+
+
+/*********************************************************/
+/***************** COMENTARIO BEA *********************/
+/*********************************************************/
+/*
+
+Resultado correcto!
+
+Resultado: OK
+Código: OK
+Legibilidad: Para mejorar la legibilidad del código te aconsejo hacer tabulaciones. La legibilidad es un punto algo más subjetivo, pero hay que intentar que sea lo más fácil de leer posible.
+              Te dejo como yo lo haría.
+
+              select c.customer_id 
+                    ,coalesce(count(distinct(order_date)),0) as Visitas
+              from case01.customers c
+              left join case01.sales s 
+                    on c.customer_id=s.customer_id
+              group by c.customer_id
+              order by visitas desc;
+
+*/
