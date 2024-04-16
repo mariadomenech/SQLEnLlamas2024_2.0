64e12359f63f0d7adc2a1421f5f7966cbc53c1fd
irenelacivica <156344357+irenelacivica@users.noreply.github.com>
2024-04-16 15:11:50 +0200

    Correción Irene Ejercicio 2.sql

--- Miguel Caso/Case 1/Ejercicio 2.sql
+++ Miguel Caso/Case 1/Ejercicio 2.sql
@@ -5,4 +5,15 @@ FROM SQL_EN_LLAMAS_ALUMNOS.CASE01.CUSTOMERS a
 LEFT JOIN SQL_EN_LLAMAS_ALUMNOS.CASE01.SALES b ON a.customer_id = b.customer_id
 GROUP BY a.customer_id;
 
-SELECT * FROM SQL_EN_LLAMAS_ALUMNOS.CASE01.SALES;
+/*********************************************************/
+/***************** COMENTARIO IRENE **********************/
+/*********************************************************/
+/*
+
+Está correcto! 
+En la última actualización de la app puse que se ordenase por num_dias de manera descendente, pero como has subido la solución antes del cambio pues....
+VALIDA LA SOLUCIÓN! jejeje
+
+Lo único nuevo es ponerle abajo del GROUP BY un ORDER BY:
+ORDER BY num_dias desc;
+*/
