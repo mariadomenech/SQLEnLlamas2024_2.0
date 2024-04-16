0ccfaf82d80662c952f787afd5190fc15a00a008
GitACastillo <105217264+GitACastillo@users.noreply.github.com>
2024-04-16 11:48:49 +0200

    Update Case02.sql

--- Carlos Rodriguez/Week 1/Case02.sql
+++ Carlos Rodriguez/Week 1/Case02.sql
@@ -3,3 +3,22 @@ COUNT(DISTINCT s.order_date) as Dias_Visitados
 FROM case01.customers c
 LEFT JOIN case01.sales s ON c.customer_id = s.customer_id
 GROUP BY c.customer_id;
+
+/*********************************************************/
+/***************** COMENTARIO ÁNGEL *********************/
+/*********************************************************/
+/*
+
+Correcto! Buen uso de Distinct para evitar contabilizar fechas repetidas.
+
+Sobre la legibilidad del código la mejoraría un poco separando los campos del select y los ONs de los joins:
+
+SELECT 
+      c.customer_id, 
+      COUNT(DISTINCT s.order_date) as Dias_Visitados
+FROM case01.customers c
+LEFT JOIN case01.sales s 
+    ON c.customer_id = s.customer_id
+GROUP BY c.customer_id;
+
+*/
