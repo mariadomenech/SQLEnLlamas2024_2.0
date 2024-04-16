700fda99600da30b0d2c57c577a096af14481440
JoseLuisMontalvo <166693694+JoseLuisMontalvo@users.noreply.github.com>
2024-04-16 09:50:27 +0200

    Update reto2.sql

--- JoseLuisMontalvo/reto2/reto2.sql
+++ JoseLuisMontalvo/reto2/reto2.sql
@@ -1,4 +1,6 @@
-select cust.customer_id as Cliente,count(distinct sal.order_date) as N_Visitas
+select cust.customer_id as Cliente
+      ,count(distinct sal.order_date) as N_Visitas
          from case01.customers cust
-             left join case01.sales sal on sal.customer_id=cust.customer_id
+         left join case01.sales sal
+	     on sal.customer_id=cust.customer_id
 group by cust.customer_id;
