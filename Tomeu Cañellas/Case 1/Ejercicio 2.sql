c517aafd67ca35d1b885899d507b93489137d97e
tomeucc <166701083+tomeucc@users.noreply.github.com>
2024-04-16 09:49:08 +0200

    Ejercicio 2.sql

--- Tomeu Cañellas/Case 1/Ejercicio 2.sql
+++ Tomeu Cañellas/Case 1/Ejercicio 2.sql
@@ -1,7 +1,7 @@
 --CASO 1: Ejercicio 2
 SELECT  customers.customer_id,
 --Cuenta de los días diferentes que ha visitado cada cliente el restaurante
-		COUNT(DISTINCT sales.order_date) AS total_visits
+	COUNT(DISTINCT sales.order_date) AS total_visits
 FROM [SQL_EN_LLAMAS_ALUMNOS].[case01].[customers] customers
 --Usamos LEFT JOIN para incluir los clientes sin registro de visitas
 LEFT JOIN [SQL_EN_LLAMAS_ALUMNOS].[case01].[sales] sales
