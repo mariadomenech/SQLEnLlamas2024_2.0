dd2d39ae59b7e155d83b01070e542ce416999ba6
DanIngeneer <156660817+DaniNgeneer@users.noreply.github.com>
2024-04-16 11:23:20 +0200

    Update Ejercicio1_2.sql

--- Ismael Navarro/WEEK 1/Ejercicio1_2.sql
+++ Ismael Navarro/WEEK 1/Ejercicio1_2.sql
@@ -7,3 +7,12 @@ FROM
         ON c.customer_id = s.customer_id
 GROUP BY 
     c.customer_id;
+
+
+
+/********************************************/
+/************COMENTARIO DANI*****************/
+/********************************************/
+/*Resultado correcto, buena legibilidad y buen manejo del DISTINCT para evitar que nos aparezca el mismo día repetidas veces,
+todo correcto, sigue así Ismael! Te animo a que busques formas originales pero no demasiado complejas para obtener el mismo
+resultado!*/
