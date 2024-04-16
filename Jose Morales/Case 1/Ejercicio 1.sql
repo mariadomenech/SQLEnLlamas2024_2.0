cf82cce531b8557d96947af79fdece79d01b3cdc
josemoes <166826124+josemoes@users.noreply.github.com>
2024-04-16 09:31:56 +0200

    Ejercicio 1.sql

--- Jose Morales/Case 1/Ejercicio 1.sql
+++ Jose Morales/Case 1/Ejercicio 1.sql
@@ -1,1 +1,10 @@
-
+SELECT 
+	customers.customer_id as Cliente,
+	ISNULL(SUM(menu.price), 0) as Total_Gasto
+FROM case01.customers customers
+LEFT JOIN case01.sales sales
+	ON customers.customer_id = sales.customer_id
+LEFT JOIN case01.menu menu
+	ON sales.product_id = menu.product_id
+GROUP BY customers.customer_id
+ORDER BY Total_Gasto DESC;
