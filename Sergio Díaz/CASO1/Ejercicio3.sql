dfb8f1fd81f1635810188e5d5b75cd7f3924bff2
sdf007 <167197927+sdf007@users.noreply.github.com>
2024-04-17 18:09:37 +0200

    Create Ejercicio3.sql

---
+++ Sergio Díaz/CASO1/Ejercicio3.sql
@@ -0,0 +1,29 @@
+/* Alumno: Sergio Díaz */
+/* CASO 1 - Ejercicio 3: Primer producto de cada cliente */
+
+SELECT principal.CLIENTE AS CLIENTE
+	,STRING_AGG(ISNULL(UPPER(LEFT(menu.product_name, 1)) + LOWER(SUBSTRING(menu.product_name, 2, LEN(menu.product_name))), ''), ', ') AS PLATO --formateo de los platos
+FROM (
+	SELECT B.DES_CLIENTE AS cliente
+		,A.product_id AS id_producto
+	FROM (
+		SELECT customers.customer_id AS des_cliente
+			,sales_detail.fecha_min AS primera_fecha
+		FROM case01.customers AS customers
+		LEFT JOIN (
+			SELECT customer_id
+				,MIN(order_date) AS fecha_min
+			FROM case01.sales sales
+			GROUP BY customer_id
+			) sales_detail ON customers.customer_id = sales_detail.customer_id
+		GROUP BY customers.customer_id
+			,sales_detail.fecha_min
+		) B --Query para obtener min fecha por cliente
+	LEFT JOIN case01.sales A ON A.customer_id = B.DES_CLIENTE
+		AND A.order_date = B.primera_fecha
+	GROUP BY B.des_cliente
+		,A.product_id
+	) principal --Join para obtener los productos en la primera fecha de cada cliente
+LEFT JOIN case01.menu --Join para obtener los nombres de los id de producto
+	ON menu.product_id = principal.id_producto
+GROUP BY principal.CLIENTE;
