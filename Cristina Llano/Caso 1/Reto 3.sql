3aee6f9a9dbc63f0b96e6d457e2f6c2008f9298c
crisllano <166803077+crisllano@users.noreply.github.com>
2024-04-17 15:02:17 +0200

    Create Reto 3.sql

---
+++ Cristina Llano/Caso 1/Reto 3.sql
@@ -0,0 +1,19 @@
+-- ¿Cuál es el primer producto que ha pedido cada cliente?
+SELECT
+	cust.customer_id
+	, COALESCE(STRING_AGG(venta.product_name, ', '), 'Sin ventas') AS product_name
+FROM SQL_EN_LLAMAS_ALUMNOS.case01.customers cust
+LEFT JOIN 
+	(
+		SELECT DISTINCT
+			sales.customer_id
+			, menu.product_name
+			, RANK() OVER( PARTITION BY sales.customer_id ORDER BY sales.order_date ASC) AS primero
+		FROM SQL_EN_LLAMAS_ALUMNOS.case01.sales sales
+		LEFT JOIN SQL_EN_LLAMAS_ALUMNOS.case01.menu menu
+			ON menu.product_id = sales.product_id
+	) venta
+	ON	venta.primero = 1
+	AND cust.customer_id = venta.customer_id
+GROUP BY cust.customer_id
+ORDER BY 1 ;
