890025f60974fd34a200bf739b939da3abd7e19e
Joram <166694672+joramarcoi@users.noreply.github.com>
2024-04-17 12:16:18 +0200

    Update Ejercicio 3.sql

--- Joram Arco/Case 1/Ejercicio 3.sql
+++ Joram Arco/Case 1/Ejercicio 3.sql
@@ -1,6 +1,6 @@
 SELECT
 	cliente,
-	COALESCE(STRING_AGG(product_name, ','),' ') as productos
+	COALESCE(STRING_AGG(product_name, ','),' ') as productos --COALESCE(STRING_AGG(product_name, ','),'Sin pedido') para sacar un literal que indique que no tiene pedidos en lugar de vacio
 FROM (
 	SELECT
 		DISTINCT customers.customer_id as cliente,
@@ -22,3 +22,5 @@ WHERE
 	ranking = 1
 GROUP BY 
 	cliente
+ORDER BY
+	cliente ASC
