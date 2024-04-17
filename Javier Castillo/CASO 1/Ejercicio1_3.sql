39a27ac0d4355cf9f4abffb586e444b89a426158
jcastillo-civica <61198075+jcastillo-civica@users.noreply.github.com>
2024-04-17 11:51:26 +0200

    Ejercicio1_3

---
+++ Javier Castillo/CASO 1/Ejercicio1_3.sql
@@ -0,0 +1,20 @@
+WITH PRODUCTOS_PEDIDOS
+AS (
+	SELECT DISTINCT C.customer_id AS CLIENTE
+	  , S.order_Date
+	  , COALESCE (M.product_name , '') AS PRODUCTO_PEDIDO
+	  , RANK() OVER (PARTITION BY C.customer_id ORDER BY S.order_Date ASC)  as RN
+	FROM [SQL_EN_LLAMAS_ALUMNOS].[case01].[customers] AS C
+	LEFT JOIN [SQL_EN_LLAMAS_ALUMNOS].[case01].[sales] AS S
+		ON C.customer_id = S.customer_id
+	LEFT JOIN [SQL_EN_LLAMAS_ALUMNOS].[case01].[menu] AS M
+		ON S.product_id=M.product_id
+)
+
+
+SELECT 
+CLIENTE
+, STRING_AGG(PRODUCTO_PEDIDO, ', ') AS PRODUCTOS_PEDIDOS
+FROM PRODUCTOS_PEDIDOS
+  WHERE RN = 1
+GROUP BY CLIENTE;
