d2c9fc0c28f2dddb2389b5e1ed6f044eea0bc36b
AnaBoschF <167062264+AnaBoschF@users.noreply.github.com>
2024-04-17 10:51:40 +0200

    Update Reto 1.sql

--- Ana Bosch/Caso 1/Reto 1.sql
+++ Ana Bosch/Caso 1/Reto 1.sql
@@ -1,4 +1,4 @@
--- CASO 1: Cuanto dinero han gastado los clientes
+-- RETO 1: Cuanto dinero han gastado los clientes
 select customers.customer_id,
 	isnull(sum(price),0) as sum_price
 from case01.sales sales
