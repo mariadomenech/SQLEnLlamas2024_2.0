9748bb1d62649caa08b763b0a8ac894c1ceadfee
Pablo Nieto <153496982+panieto@users.noreply.github.com>
2024-04-17 17:49:42 +0200

    Comentarios Pablo

--- JairoAbril/Caso 1/Ejercicio1_2.sql
+++ JairoAbril/Caso 1/Ejercicio1_2.sql
@@ -6,3 +6,13 @@ LEFT JOIN SQL_EN_LLAMAS_ALUMNOS.CASE01.SALES SALES
   ON CUSTOMERS.CUSTOMER_ID = SALES.CUSTOMER_ID
 GROUP BY CUSTOMERS.CUSTOMER_ID
 ORDER BY NUM_VISITAS DESC;      // se utiliza para mostrar un ranking de los clientes con más visitas
+
+/*
+Corrección Pablo: Todo perfecto.
+
+Resultado: OK. Obtienes justo lo que se pedía.
+Código: OK. Bien aplicado el COUNT DISTINCT.
+Legibilidad: OK. Esto es más subjetivo, pero a mí me gusta como has presentado el ejercicio.
+
+Además, me ha gustado que ordenes la salida. ¡Enhorabuena!
+*/
