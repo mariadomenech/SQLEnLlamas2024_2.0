9fc629e5531e94385747d745d3f5354ef2059e4e
tomeucc <166701083+tomeucc@users.noreply.github.com>
2024-04-17 15:18:03 +0200

    Ejercicio 3.sql

--- Tomeu Cañellas/Case 1/Ejercicio 3.sql
+++ Tomeu Cañellas/Case 1/Ejercicio 3.sql
@@ -1,11 +1,11 @@
 --CASO 1: Ejercicio 3
 SELECT  part.customer_id,
-		STRING_AGG(ISNULL(menu.product_name, 'none'), ', ') AS first_order -- Agregamos los productos de un mismo pedido y sustituimos por 'none' los valores nulos
+	STRING_AGG(ISNULL(menu.product_name, 'none'), ', ') AS first_order -- Agregamos los productos de un mismo pedido y sustituimos por 'none' los valores nulos
 FROM (
 	-- Creamos una subconsulta 'part' con agrupaciones por cliente, cada una ordenada por fecha de pedido de forma ascendente
 	SELECT DISTINCT customers.customer_id AS customer_id,
-					sales.product_id AS product_id,
-					RANK() OVER (PARTITION BY customers.customer_id ORDER BY sales.order_date ASC) AS ranking
+			sales.product_id AS product_id,
+			RANK() OVER (PARTITION BY customers.customer_id ORDER BY sales.order_date ASC) AS ranking
 	FROM [SQL_EN_LLAMAS_ALUMNOS].[case01].[customers] customers
 	LEFT JOIN [SQL_EN_LLAMAS_ALUMNOS].[case01].[sales] sales
 		ON customers.customer_id = sales.customer_id
