607021cfa83d61da5134e1baa0b65b2aca5721eb
DanIngeneer <156660817+DaniNgeneer@users.noreply.github.com>
2024-04-17 09:58:46 +0200

    Update Day2.sql

--- Paco Mantell/Week1/Day2.sql
+++ Paco Mantell/Week1/Day2.sql
@@ -5,3 +5,12 @@ LEFT JOIN case01.sales B
 	ON A.customer_id=B.customer_id
 GROUP BY A.customer_id
 ORDER BY 2 DESC;
+
+
+
+/*******************************************/
+/***********COMENTARIO DANI*****************/
+/*******************************************/
+/*Resultado correcto, buen uso de DISTINCT para evitar que nos aparezca el mismo día repetidas veces, 
+muy buen uso de manejo de nulos. Buen enfoque al cruce de tablas. Te animo a que intentes buscar 
+formas un poco mas complejillas para llegar a los mismos resultados, aún así muy buen trabajo!!*/
