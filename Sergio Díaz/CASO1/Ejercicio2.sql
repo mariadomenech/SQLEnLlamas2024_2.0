ad96f87f2870d775031dca610e71bd1960b9b597
sdf007 <167197927+sdf007@users.noreply.github.com>
2024-04-17 13:24:41 +0200

    Create Ejercicio2.sql

---
+++ Sergio Díaz/CASO1/Ejercicio2.sql
@@ -0,0 +1,15 @@
+/* Alumno: Sergio Díaz */
+/* CASO 1 - Ejercicio 2: Total de días de visita al restaurante de cada cliente */
+
+SELECT 
+   A.customer_id as 'DES_CLIENTE',
+   COALESCE( count(distinct B.order_date), 0) AS 'TOTAL_DIAS' --Metrica para contar dias distintos
+FROM 
+   case01.customers A 
+      LEFT JOIN --Left join para mostrar todos los clientes de la dimension cliente
+         case01.sales B
+      ON A.customer_id = B.customer_id
+GROUP BY 
+   A.customer_id
+ORDER BY 
+   TOTAL_DIAS DESC; --Ordenamos por numero de dias desc
