1da328cffc77bd4e1f922a1e2295f2ae4401e1bc
Maria <61223904+mariadomenech@users.noreply.github.com>
2024-04-18 16:19:49 +0200

    Corrección Ejercicio 4.sql

--- Regina Margolles/Caso 1/Ejercicio 4.sql
+++ Regina Margolles/Caso 1/Ejercicio 4.sql
@@ -5,3 +5,20 @@ from case01.sales as s
 			    on s.product_id = m.product_id
 group by s.product_id, m.product_name 
 order by COUNT(m.product_id) desc;
+
+
+
+/*********************************************************/
+/***************** COMENTARIO MARÍA *********************/
+/*********************************************************/
+
+/* 
+
+Perfecto Regina! 
+Las tabulaciones las cambiaría por lo que te he comnetado en los ejericios 1 y 2 de legibilidad.
+
+Como consejo: El top 1 solo te va a devolver un único resultado o línea, ¿qué ocurre si existe empate en el número de veces pedido dos productos? 
+Habría que usar la función RANK(), que si para una misma partición, dos valores son iguales al ordenarlos, tienen el mismo rango o número y 
+nos permite sacar más de un producto en caso de empate.
+
+*/
