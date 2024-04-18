27a06e66387606b4d32a3d3938daa717ead3b156
Monticius <167069808+Monticius@users.noreply.github.com>
2024-04-18 16:32:07 +0200

    Add files via upload

---
+++ Jose Montero/Caso 01/Caso01_reto4.sql
@@ -0,0 +1,14 @@
+select
+	ss.Producto
+	, ss.Num_Ventas
+from (select
+		 ms.product_name as Producto
+		 , count(vs.product_id) as Num_Ventas
+	 from case01.sales vs
+	 inner join case01.menu ms
+		 on vs.product_id=ms.product_id
+	 group by ms.product_name) ss
+where ss.Num_Ventas=(select top 1 count(s.product_id)
+					from case01.sales s
+					group by s.product_id
+					order by 1 desc)
