dbd8ef6a2fd46b5ed52d3e7c3e602be135cc4e99
Jose Sanchez Robles <josesanxez@gmail.com>
2024-04-18 08:54:41 +0200

    Update Ejercicio 3.sql

--- Jose Sanchez/Case 1/Ejercicio 3.sql
+++ Jose Sanchez/Case 1/Ejercicio 3.sql
@@ -1,21 +1,39 @@
 WITH CTE as (
     SELECT
-        *,
-        ROW_NUMBER() OVER (
-            PARTITION BY customer_id
-            ORDER BY
-                order_date ASC
-        ) AS row_number
+        customer_id,
+        MIN(order_date) as order_date
     FROM
-        case01.sales
+        case01.sales as sales
+    GROUP BY
+        sales.customer_id
+),
+CTE_first_sales as (
+    SELECT
+        sales.customer_id,
+        menu.product_name
+    from
+        case01.sales as sales
+        RIGHT JOIN CTE as first_sale ON sales.customer_id = first_sale.customer_id
+        AND sales.order_date = first_sale.order_date
+        RIGHT JOIN case01.menu as menu ON menu.product_id = sales.product_id
 )
 SELECT
-    customers.customer_id,
-    menu.product_name,
-    sales.order_date
-FROM
-    case01.customers as customers
-    LEFT JOIN CTE as sales ON customers.customer_id = sales.customer_id
-    LEFT JOIN case01.menu as menu ON menu.product_id = sales.product_id
-WHERE
-    sales.row_number = 1
+    [customer_id],
+    ISNULL(
+        STUFF(
+            (
+                SELECT
+                    DISTINCT ', ' + product_name
+                FROM
+                    CTE_first_sales as sales
+                WHERE
+                    customers.customer_id = sales.customer_id FOR XML PATH ('')
+            ),
+            1,
+            1,
+            ''
+        ),
+        ''
+    ) AS products
+from
+    case01.customers as customers;
