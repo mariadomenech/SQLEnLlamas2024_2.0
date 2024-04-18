1038bab4f67ff48b0fa8af3dfa2acdae4a2f3941
JairoAbril <166705520+JairoAbril@users.noreply.github.com>
2024-04-18 12:36:42 +0200

    Update Ejercicio1_4.sql

--- JairoAbril/Caso 1/Ejercicio1_4.sql
+++ JairoAbril/Caso 1/Ejercicio1_4.sql
@@ -1,1 +1,21 @@
 
+WITH NUM_PEDIDOS_PRODUCTOS AS (
+    SELECT 
+        m.PRODUCT_NAME, 
+        COUNT(*) AS PedidosTotales
+    FROM case01.sales s
+    JOIN case01.menu m ON s.PRODUCT_ID = m.PRODUCT_ID
+    GROUP BY m.PRODUCT_NAME
+),
+Ranking_Productos AS (
+    SELECT 
+        PRODUCT_NAME, 
+        PedidosTotales, 
+        RANK() OVER (ORDER BY PedidosTotales DESC) AS Ranking
+    FROM NUM_PEDIDOS_PRODUCTOS 
+)
+SELECT 
+    PRODUCT_NAME, 
+    PedidosTotales
+FROM Ranking_Productos 
+WHERE Ranking = 1;
