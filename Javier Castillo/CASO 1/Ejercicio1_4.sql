a8064ba07daf2781e48586ed7bbf6b13fddbeee8
jcastillo-civica <61198075+jcastillo-civica@users.noreply.github.com>
2024-04-18 09:50:47 +0200

    Update Ejercicio1_4.sql

--- Javier Castillo/CASO 1/Ejercicio1_4.sql
+++ Javier Castillo/CASO 1/Ejercicio1_4.sql
@@ -1,5 +1,6 @@
 SELECT TOP 1
-M.product_name, COUNT(1) AS NUM_PEDIDOS
+M.product_name
+, COUNT(1) AS NUM_PEDIDOS
 FROM [SQL_EN_LLAMAS_ALUMNOS].[case01].[sales] AS S
 LEFT JOIN [SQL_EN_LLAMAS_ALUMNOS].[case01].[menu] AS M
 	ON S.product_id=M.product_id
