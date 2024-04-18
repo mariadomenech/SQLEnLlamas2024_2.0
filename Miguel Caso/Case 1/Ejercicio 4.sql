a4e2c8d53487882cc734619d9eeaf23a8f422368
irenelacivica <156344357+irenelacivica@users.noreply.github.com>
2024-04-18 13:41:22 +0200

    Corrección Irene Ejercicio 4.sql

--- Miguel Caso/Case 1/Ejercicio 4.sql
+++ Miguel Caso/Case 1/Ejercicio 4.sql
@@ -7,3 +7,28 @@ SELECT m.product_name, t.num_pedidos
     JOIN SQL_EN_LLAMAS.CASE01.MENU m ON t.product_id = m.product_id
     WHERE t.num_pedidos = (SELECT MAX(num_pedidos) FROM total_pedidos)
     ORDER BY m.product_name;
+
+/*********************************************************/
+/***************** COMENTARIO IRENE **********************/
+/*********************************************************/
+/*
+RESULTADO: Correcto
+CÓDIGO: Correcto
+LEGIBILIDAD: Correcta
+
+Perfecto Miguel!!!
+
+Te enseño otra solución para que veas otra forma de hacer el ejercicio sin tener que hacer una subconsulta:
+SELECT
+    TOP(1) menu.product_name,
+    COUNT(sales.product_id) as units_sold
+from
+    case01.menu as menu
+    LEFT JOIN case01.sales as sales ON menu.product_id = sales.product_id
+GROUP BY
+    sales.product_id,
+    menu.product_name
+ORDER BY
+    units_sold DESC;
+
+*/
