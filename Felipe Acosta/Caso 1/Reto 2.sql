4803ff661468f15b5216440aa8c086cfd9bee0b5
DanIngeneer <156660817+DaniNgeneer@users.noreply.github.com>
2024-04-18 12:45:15 +0200

    Update Reto 2.sql

--- Felipe Acosta/Caso 1/Reto 2.sql
+++ Felipe Acosta/Caso 1/Reto 2.sql
@@ -1,8 +1,16 @@
 -- RETO 2
--- �C�antos dias ha visitado el restaurante cada cliente?
+-- ¿Cúantos dias ha visitado el restaurante cada cliente?
 
 SELECT A.customer_id Cliente, count(distinct B.order_date) Dias_visitados
 FROM SQL_EN_LLAMAS_ALUMNOS.case01.customers A
 LEFT JOIN SQL_EN_LLAMAS_ALUMNOS.case01.sales B ON (A.customer_id = B.customer_id)
 group by A.customer_id
 order by 2 desc;
+
+/********************************************/
+/************COMENTARIO DANI*****************/
+/********************************************/
+/* Resultado correcto, buen apuntado de tablas, lo único sería 
+mejorar la legibilidad del código, te animo a que lo corrijas 
+y me lo dejes bajo el comentario Felipe!. Quitando ese detallito 
+todo correcto! Enhorabuena y a por mas!*/
