611bbd824860571a878493f16dbea9020b37410d
Felipe <fjad20@gmail.com>
2024-04-18 11:32:18 +0200

    Update Reto 1.sql
    ;
    1 cliente faltante

--- Felipe Acosta/Caso 1/Reto 1.sql
+++ Felipe Acosta/Caso 1/Reto 1.sql
@@ -1,11 +1,12 @@
 -- RETO 1
--- ¿Cúanto ha gasto en total cada cliente en el restaurante?
+-- Â¿CÃºanto ha gasto en total cada cliente en el restaurante?
 
 WITH sales_with_price AS(
-	SELECT A.customer_id, A.order_date , A.product_id, B.price
-	FROM SQL_EN_LLAMAS_ALUMNOS.case01.sales A
-	LEFT JOIN SQL_EN_LLAMAS_ALUMNOS.case01.menu B ON (A.product_id= B.product_id)
+	SELECT A.customer_id, B.order_date , B.product_id, ISNULL(C.price, 0) price
+	FROM SQL_EN_LLAMAS_ALUMNOS.case01.customers A
+	LEFT JOIN SQL_EN_LLAMAS_ALUMNOS.case01.sales B ON (A.customer_id = B.customer_id)
+	LEFT JOIN SQL_EN_LLAMAS_ALUMNOS.case01.menu C ON (B.product_id= C.product_id)
 )
 SELECT customer_id Cliente, SUM(price) Total_gastado
 FROM sales_with_price
-GROUP BY customer_id
+GROUP BY customer_id;
