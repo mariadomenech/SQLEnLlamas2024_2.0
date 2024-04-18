dc36d43639d82be706c02add51160b4000d03a22
DanIngeneer <156660817+DaniNgeneer@users.noreply.github.com>
2024-04-18 11:59:52 +0200

    Update Ejercicio1_4.sql

--- Ismael Navarro/WEEK 1/Ejercicio1_4.sql
+++ Ismael Navarro/WEEK 1/Ejercicio1_4.sql
@@ -11,3 +11,16 @@ WITH ProductosPedidos AS (
 SELECT TOP 1 WITH TIES PRODUCT_NAME, PedidosTotales
 FROM ProductosPedidos
 ORDER BY PedidosTotales DESC;
+
+/****************************************/
+/************COMENTARIO DANI*************/
+/****************************************/
+/*
+Resultado correcto, buena legibilidad, me gusta el enfoque que le has dado al usar
+una función como WITH TIES, sin embargo aunque en este caso esté completamente correcto
+debes tener en cuenta que la función WITH TIES devolverá todos los productos que tengan
+la misma cantidad de ventas que el producto con la cantidad mas alta, Si hay empates 
+en la cantidad de pedidos (varios productos con la misma cantidad), WITH TIES 
+incluirá todos los productos empatados en el resultado final. Aún así, te animo a que 
+continues por estas sendas de exploración en este maravilloso mundo del SQL, sigue así
+Ismael! Tu puedes!!*/
