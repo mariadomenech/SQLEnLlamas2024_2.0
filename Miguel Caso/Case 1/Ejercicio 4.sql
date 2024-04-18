55274ba39d8f36b133d54b2fd41987b627df31d0
MiguelCasoLassaletta <166695557+MiguelCasoLassaletta@users.noreply.github.com>
2024-04-18 09:35:16 +0200

    Update Ejercicio 4.sql

--- Miguel Caso/Case 1/Ejercicio 4.sql
+++ Miguel Caso/Case 1/Ejercicio 4.sql
@@ -2,7 +2,8 @@ WITH total_pedidos AS (
     SELECT product_id, COUNT (product_id) AS num_pedidos
     FROM SQL_EN_LLAMAS.CASE01.SALES s
     GROUP BY product_id)
-SELECT TOP 1 m.product_name, t.num_pedidos
+SELECT m.product_name, t.num_pedidos
     FROM total_pedidos t
     JOIN SQL_EN_LLAMAS.CASE01.MENU m ON t.product_id = m.product_id
-    ORDER BY t.num_pedidos DESC;
+    WHERE t.num_pedidos = (SELECT MAX(num_pedidos) FROM total_pedidos)
+    ORDER BY m.product_name;
