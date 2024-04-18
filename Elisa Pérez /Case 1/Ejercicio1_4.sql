73409d60b1445d6ed4ffd6ce4fd176bd451c108d
DanIngeneer <156660817+DaniNgeneer@users.noreply.github.com>
2024-04-18 11:47:27 +0200

    Update Ejercicio1_4.sql

--- Elisa Pérez /Case 1/Ejercicio1_4.sql
+++ Elisa Pérez /Case 1/Ejercicio1_4.sql
@@ -6,3 +6,11 @@ LEFT JOIN case01.menu M
   ON S.product_id = M.product_id
 GROUP BY M.product_name
 ORDER BY total DESC
+
+
+/**************************************/
+/********COMENTARIO DANI***************/
+/**************************************/
+/* Resultado correcto, legibilidad impoluta y atajas con un TOP 1, me gusta el enfoque
+Elisa!.Te animo a buscar alternativas al uso del TOP, como una función de ventana RANK.
+Ánimo y sigue así, este es el camino!*/
