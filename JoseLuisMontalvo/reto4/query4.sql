14289dac4c47ff724c9b31558ce05a545592cfb5
JoseLuisMontalvo <166693694+JoseLuisMontalvo@users.noreply.github.com>
2024-04-18 10:18:06 +0200

    Create query4.sql

---
+++ JoseLuisMontalvo/reto4/query4.sql
@@ -0,0 +1,11 @@
+/****** Cual es el producto más solicitado y el nº de veces solicitado ********/
+
+select Ranking.Producto_mas_solicitado
+      ,Numero_solicitudes from ( select men.product_name as Producto_mas_solicitado,
+                                                 count(*) as Numero_solicitudes,
+												 RANK() OVER (ORDER BY count(*) DESC) as Rango
+			                     from case01.sales sal
+                                 inner join case01.menu men 
+								 on sal.product_id=men.product_id
+                                 group by men.product_name) as Ranking
+where Ranking.Rango=1;
