63d3d24d16181da6c417e8aaa5e28a478897a9c8
Manuel Reina <156662906+manuureina@users.noreply.github.com>
2024-04-19 08:29:56 +0200

    Corrección Ejercicio2.sql

--- Pablo Villanueva/Week 1/Ejercicio2.sql
+++ Pablo Villanueva/Week 1/Ejercicio2.sql
@@ -5,3 +5,16 @@ LEFT JOIN case01.sales S
 	ON C.customer_id = S.customer_id
 GROUP BY C.customer_id 
 ;
+
+
+
+/*********************************************************/
+/***************** COMENTARIO MANU *********************/
+/*********************************************************/
+/*
+
+Perfecto Pablo! El resultado  y el código es correcto, por comentar algo se podría ordenar por
+el número de visitas, a seguir así!!
+
+
+*/
