fa0104571d414292c6eb2d5cfb9a003e22181d03
GitACastillo <105217264+GitACastillo@users.noreply.github.com>
2024-04-19 11:56:42 +0200

    correccion Ejercicio_4.sql

--- Carlos Rodriguez/Week 1/Ejercicio_4.sql
+++ Carlos Rodriguez/Week 1/Ejercicio_4.sql
@@ -13,3 +13,21 @@ GROUP BY
  menu.product_name
 ORDER BY 
  COUNT(*) DESC;
+
+/*********************************************************/
+/***************** COMENTARIO ÁNGEL *********************/
+/*********************************************************/
+/*
+
+El resultado es correcto, aunque se podria mejorar un poco la legibilidad evitando tantos saltos de linea:
+
+  SELECT TOP 1 
+        menu.product_name AS "Producto más pedido",
+        COUNT(*) AS "Veces pedido"
+  FROM case01.sales sales
+  JOIN case01.menu menu
+    ON sales.product_id = menu.product_id
+  GROUP BY menu.product_name
+  ORDER BY COUNT(*) DESC;
+
+*/
