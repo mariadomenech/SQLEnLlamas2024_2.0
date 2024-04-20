862debfa82b2df607d8bc1b92a14f9420a8cbf0e
beatrizexposito <156438327+beatrizexposito@users.noreply.github.com>
2024-04-20 18:35:52 +0200

    Comentario Ejercicio1_4.sql

--- David Garcia/week_1/Ejercicio1_4.sql
+++ David Garcia/week_1/Ejercicio1_4.sql
@@ -14,3 +14,42 @@ from(
 	  ) as conteo 
 group by product_name
 order by total_x_product desc;
+
+
+SELECT TOP (1)
+  M.PRODUCT_NAME PRODUCTO,
+  COUNT(S.PRODUCT_ID) VECES_PEDIDO
+FROM CASE01.MENU M
+JOIN CASE01.SALES S
+  ON M.PRODUCT_ID = S.PRODUCT_ID
+GROUP BY M.PRODUCT_NAME
+ORDER BY VECES_PEDIDO DESC;
+
+
+/*********************************************************/
+/***************** COMENTARIO BEA *********************/
+/*********************************************************/
+/*
+
+Resultado correcto, enhorabuena David!
+
+Resultado: OK
+Código: En este caso el resultado sale correcto utilizando un TOP, pero no nos sirve para ver cuando haya empates entre productos más vendidos.
+	Para tener en cuenta esta casuística, podríamos utilizar un RANK. Te dejo una forma de hacerlo:
+
+		SELECT 
+			data.product_name
+			,data.num_ventas
+		FROM
+		(
+			SELECT 
+				 menu.product_name
+				,COUNT(sales.customer_id) AS num_ventas
+				,RANK() OVER( ORDER BY COUNT(sales.customer_id) DESC) AS rank_ventas
+			FROM SQL_EN_LLAMAS_ALUMNOS.case01.sales sales
+			INNER JOIN SQL_EN_LLAMAS_ALUMNOS.case01.menu menu
+				ON menu.product_id = sales.product_id
+			GROUP BY  menu.product_name
+		) data
+		WHERE rank_ventas = 1;
+Legibilidad: OK. 
