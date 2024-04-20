62a92ea60d54e9470ab9880bb286ff11f3cc7ef1
beatrizexposito <156438327+beatrizexposito@users.noreply.github.com>
2024-04-20 18:48:44 +0200

    Comentario Reto 5.sql

--- Cristina Llano/Caso 1/Reto 5.sql
+++ Cristina Llano/Caso 1/Reto 5.sql
@@ -11,3 +11,31 @@ LEFT JOIN SQL_EN_LLAMAS_ALUMNOS.case01.menu menu
 	ON menu.product_id = sales.product_id
 GROUP BY cust.customer_id
 ORDER BY 1;
+
+
+/*********************************************************/
+/***************** COMENTARIO BEA *********************/
+/*********************************************************/
+/*
+
+Todo correcto, enhorabuena!!
+
+Resultado: OK
+Código: OK.
+Legibilidad: OK. Un pequeño detalle, cuando utilizamos CASE WHEN cada WHEN (o el ELSE en este caso) yo lo suelo alinear debajo del anterior WHEN. Te muestro como lo haría:
+			SELECT 
+			  cust.customer_id
+			  , COALESCE(SUM(CASE WHEN menu.product_name = 'sushi' then menu.price * 20 
+					      ELSE menu.price * 10 END)
+			            , 0) AS puntos_fidelizacion
+			FROM SQL_EN_LLAMAS_ALUMNOS.case01.customers cust
+			LEFT JOIN SQL_EN_LLAMAS_ALUMNOS.case01.sales sales
+			  ON cust.customer_id = sales.customer_id
+			LEFT JOIN SQL_EN_LLAMAS_ALUMNOS.case01.menu menu
+				ON menu.product_id = sales.product_id
+			GROUP BY cust.customer_id
+			ORDER BY 1;
+
+
+
+*/
