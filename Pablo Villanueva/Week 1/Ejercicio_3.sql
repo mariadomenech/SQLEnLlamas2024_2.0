4ebed558d5f68aead17228236e88eec5a1951fed
Manuel Reina <156662906+manuureina@users.noreply.github.com>
2024-04-20 18:16:54 +0200

    Corrección Ejercicio_3.sql

--- Pablo Villanueva/Week 1/Ejercicio_3.sql
+++ Pablo Villanueva/Week 1/Ejercicio_3.sql
@@ -19,3 +19,24 @@ LEFT JOIN
     [case01].[sales] s ON c.customer_id = s.customer_id AND pv.primera_venta_date = s.order_date
 LEFT JOIN 
     [case01].[menu] m ON s.product_id = m.product_id;
+
+
+/*********************************************************/
+/***************** COMENTARIO MANU *********************/
+/*********************************************************/
+/*
+La solución al ejercicio y por tanto el código del mismo no es correcta del todo. La idea es buena
+y me gusta la creación de la CTE para quedarte con la fecha mínima de pedido de cada cliente.
+
+Sin embargo, no es correcta ya que al usar FIRST_VALUE como función ventana te estás quedando únicamente con el
+primer valor encontrado, pero no se está teniendo en cuenta empates y es que el cliente A en la misma fecha (fecha mínima)
+pidió 2 productos distintos. 
+
+La idea de este ejercicio era usar un RANK O DENSE_RANK y el STRING_AGG para agrupar distintos productos en una lista
+y así sea más limpia la salida.
+
+Échale un vistazo y cualquier cosa puedes preguntar sin problema.
+Además te recomiendo echarle un vistazo a la app de streamlit https://sqlonfire.civica-soft.com/
+para comparar tu resultado con el esperado :).
+
+*/
