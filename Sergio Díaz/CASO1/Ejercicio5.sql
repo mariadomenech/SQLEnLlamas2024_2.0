b220aeaeb0c9c53479891f529d42ce223a479612
sdf007 <167197927+sdf007@users.noreply.github.com>
2024-04-21 20:40:17 +0200

    Create Ejercicio5.sql
    Ejercicio 5 listo para revisión

---
+++ Sergio Díaz/CASO1/Ejercicio5.sql
@@ -0,0 +1,15 @@
+/* Alumno: Sergio Díaz */
+/* CASO 5: Total de puntos de fidelización de cada cliente */
+
+
+SELECT CUSTOMERS.customer_id AS 'DES_CLIENTE'
+	,COALESCE(SUM(CASE 
+				WHEN SALES.product_id = 1
+					THEN MENU.price * 20
+				ELSE MENU.price * 10
+				END), 0) AS TOTAL_PUNTOS --case para obtener los puntos en funcion del plato (el sushi id=1 es el doble)
+FROM case01.customers CUSTOMERS
+LEFT JOIN case01.sales SALES ON CUSTOMERS.customer_id = SALES.customer_id --Left join para obtener todos los clientes (aunque no aparezcan en la tabla de sales)
+LEFT JOIN case01.menu MENU 
+	ON SALES.product_id = MENU.product_id 
+GROUP BY CUSTOMERS.customer_id;
