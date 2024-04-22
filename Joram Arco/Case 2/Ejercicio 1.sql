0d051ece9d2fa9b52383ec62d02f4d7b3241f0eb
Joram <166694672+joramarcoi@users.noreply.github.com>
2024-04-22 14:24:43 +0200

    Create Ejercicio 1.sql

---
+++ Joram Arco/Case 2/Ejercicio 1.sql
@@ -0,0 +1,23 @@
+WITH runner_orders AS(
+	SELECT
+		runner_id,
+		order_id,
+		pickup_time,
+		COALESCE(SUM(CAST(LEFT(distance, PATINDEX('%[^0-9.]%', distance + ' ') -1 ) AS FLOAT)), 0) as distance, --Nos quedamos solo con la parte numérica del campo y la convertimos en float
+		COALESCE(SUM(CAST(LEFT(duration, PATINDEX('%[^0-9.]%', duration + ' ') -1 ) AS FLOAT))/60, 0) as duration --Igual que el anterior, pero además dividimos por 60 para quedarnos con las horas
+	FROM case02.runner_orders 
+	GROUP BY runner_id,
+		order_id,
+		pickup_time
+)
+
+SELECT
+	runners.runner_id,
+	COALESCE(SUM(distance),0) as distancia_acumulada_km, --Evitamos los nulos poniendo un 0 en su lugar
+	ROUND(COALESCE(AVG( CASE WHEN duration > 0 THEN distance/duration
+						ELSE NULL END), 0), 4) as velocidad_promedio_km_h --Redondeamos a 4 decimales y evitamos la división por 0
+FROM runner_orders runner_orders
+RIGHT JOIN case02.runners runners
+		ON (runner_orders.runner_id = runners.runner_id)
+
+GROUP BY runners.runner_id;
