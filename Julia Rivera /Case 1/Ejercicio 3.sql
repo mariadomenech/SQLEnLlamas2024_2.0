0e1a3ce9f66cb486a10e9b29fc75540a149515bf
irenelacivica <156344357+irenelacivica@users.noreply.github.com>
2024-04-22 14:57:16 +0200

    Corrección Irene - Ejercicio 3.sql

--- Julia Rivera /Case 1/Ejercicio 3.sql
+++ Julia Rivera /Case 1/Ejercicio 3.sql
@@ -15,3 +15,36 @@ FROM (
 		ON S.PRODUCT_ID = M.PRODUCT_ID
 ) AS SUBCONSULTA
 WHERE ROW_NUM = 1;
+
+/*********************************************************/
+/***************** COMENTARIO IRENE **********************/
+/*********************************************************/
+/*
+RESULTADO: Incorrecto
+CÓDIGO: Estás utilizando row_number pero habría que usar rank, intenta usar más left o right joins. 
+	Cuando hagas una subconsulta intenta ponerlas en una CTE para que se quede más limpito el código
+LEGIBILIDAD: Correcta
+
+
+AQUÍ TIENES UN EJEMPLO DE CÓMO SE PODRÍA HACER:
+______________________________________________
+WITH CTE AS(
+    SELECT
+        DISTINCT me.customer_id cliente,
+        m.product_name, 
+        s.order_date,
+        RANK() OVER(PARTITION BY me.customer_id ORDER BY s.order_date) AS SEQ
+    FROM SQL_EN_LLAMAS.CASE01.CUSTOMERS me
+        LEFT JOIN SQL_EN_LLAMAS.CASE01.SALES s ON me.customer_id = s.customer_id
+        LEFT JOIN SQL_EN_LLAMAS.CASE01.MENU m ON s.product_id = m.product_id
+) 
+SELECT 
+     cliente,
+     COALESCE(STRING_AGG(product_name, ', '),' ') AS "PRIMER PRODUCTO QUE HA PEDIDO"
+FROM CTE
+WHERE SEQ = 1
+GROUP BY cliente
+ORDER BY cliente;
+
+*/
+
