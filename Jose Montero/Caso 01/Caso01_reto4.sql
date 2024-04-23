75714bdac236e454ff476ba9e59d6fb4e6edf040
Maria <61223904+mariadomenech@users.noreply.github.com>
2024-04-23 10:48:03 +0200

    Corrección Caso01_reto4.sql

--- Jose Montero/Caso 01/Caso01_reto4.sql
+++ Jose Montero/Caso 01/Caso01_reto4.sql
@@ -19,11 +19,7 @@ where ss.Num_Ventas=(select top 1 count(s.product_id)
 
 /* 
 
-Resultado OK!
-
-Como consejo: El top 1 solo te va a devolver un único resultado o línea, ¿qué ocurre si existe empate entre dos productos en el número de veces que se han pedido? 
-Haría uso de la función RANK(), por ejemplo, que si para una misma partición, dos valores son iguales al ordenarlos, tienen el mismo rango o número y 
-nos permite sacar más de un producto en caso de empate.
+Resultado y código OK!
 
 Legibilidad: Ok.
 
