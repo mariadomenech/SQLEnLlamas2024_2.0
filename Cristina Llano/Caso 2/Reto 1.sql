0d4589cb295be0e829b3c86d717e1ed0f0b21d97
crisllano <166803077+crisllano@users.noreply.github.com>
2024-04-23 12:59:56 +0200

    Update Reto 1.sql

--- Cristina Llano/Caso 2/Reto 1.sql
+++ Cristina Llano/Caso 2/Reto 1.sql
@@ -4,27 +4,28 @@
 --Se realiza limpieza de datos además de calculos base de sumatorios y transformación a tipos de datos correctos
 WITH tmp_runner_orders AS(
 	SELECT runner_id
-		, SUM( CAST (CASE WHEN (distance is null OR distance = 'null') THEN '0'
+		, CAST (CASE WHEN (distance is null OR distance = 'null') THEN '0'
 					 ELSE REPLACE(distance, 'km','') END
-				AS DECIMAL(3,1))) AS distancia_total
-		, SUM( CAST (CASE WHEN (duration is null OR duration = 'null') THEN '0'
+				AS DECIMAL(4,2)) AS distancia_total
+		, CAST (CASE WHEN (duration is null OR duration = 'null') THEN '0'
 					 ELSE SUBSTRING(duration,1,2) END 
-				AS DECIMAL(4,2)))/60 AS tiempo_total_hora
+				AS DECIMAL(4,2))/60 AS tiempo_total_hora
 	FROM SQL_EN_LLAMAS_ALUMNOS.case02.runner_orders
-	GROUP BY runner_id
+
 )
 
 -- Para que quede más legible se incluye el tipo de información que se aporta, km o km/h
 SELECT runner
-	, CONCAT (distancia_acumulada, ' km') AS distancia_acumulada
-	, CONCAT (velocidad_promedio, ' km/h') AS velocidad_promedio
+	, CONCAT (distancia_acumulada, ' KM') AS distancia_acumulada
+	, CONCAT (velocidad_promedio, ' KM/H') AS velocidad_promedio
 FROM
 (
 	SELECT run.runner_id AS runner
-		, COALESCE(MAX(reparto.distancia_total),0) AS distancia_acumulada
-		, CAST(COALESCE(AVG(reparto.distancia_total / reparto.tiempo_total_hora),0) AS DECIMAL (4,2)) AS velocidad_promedio
+		, COALESCE(SUM(reparto.distancia_total),0) AS distancia_acumulada
+		, CAST( COALESCE(AVG(reparto.distancia_total / reparto.tiempo_total_hora),0) AS DECIMAL (4,2)) AS velocidad_promedio
 	FROM SQL_EN_LLAMAS_ALUMNOS.case02.runners run
 	LEFT JOIN tmp_runner_orders reparto
 		ON run.runner_id = reparto.runner_id
+		AND reparto.tiempo_total_hora > 0 -- Excluimos valor cero para no dividir por él
 	GROUP BY run.runner_id
 ) FINAL ;
