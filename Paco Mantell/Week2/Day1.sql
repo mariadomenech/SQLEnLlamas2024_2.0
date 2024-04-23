e8a843a90d5ac0254eb803d04a0e40bacb66bbd7
pacomantell <109753909+pacomantell@users.noreply.github.com>
2024-04-23 19:28:10 +0200

    Create Day1.sql

---
+++ Paco Mantell/Week2/Day1.sql
@@ -0,0 +1,19 @@
+WITH CTE_CLEAN AS(
+	SELECT A.runner_id runner,
+		CASE WHEN PATINDEX('%[A-z]%', B.distance) > 0 THEN
+			CAST(LEFT(B.distance, PATINDEX('%[A-z]%', B.distance) - 1) AS FLOAT)
+			ELSE ISNULL(CAST(B.distance AS FLOAT), 0)
+		END distance,
+		CASE WHEN PATINDEX('%[a-z]%', B.duration) > 0 THEN
+			CAST(LEFT(B.duration, PATINDEX('%[a-z]%', B.duration) - 1) AS FLOAT)
+		ELSE ISNULL(CAST(B.duration AS FLOAT), 0)
+		END duration
+	FROM case02.runners A
+	LEFT JOIN case02.runner_orders B
+		ON A.runner_id = B.runner_id
+)
+SELECT runner,
+  CONCAT(SUM(distance), 'km') total_dist,
+  CONCAT(ISNULL(CAST(AVG(distance * 1000 / NULLIF(duration * 60, 0))AS DECIMAL(5,2)),0), 'm/s') avg_vel
+FROM CTE_CLEAN
+GROUP BY runner;
