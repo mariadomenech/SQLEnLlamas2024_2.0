fb2277e115f5e100f0077a82c94e2e2f29845e0f
DanIngeneer <156660817+DaniNgeneer@users.noreply.github.com>
2024-04-23 10:04:23 +0200

    Update Ejercicio2_1.sql

--- Elisa Pérez /Case 2/Ejercicio2_1.sql
+++ Elisa Pérez /Case 2/Ejercicio2_1.sql
@@ -12,3 +12,17 @@ SELECT	runner_id
 		    , ISNULL(CAST(AVG(distance/duration)AS decimal(4,2)), 0)  velocidad_avg
 FROM tmp
 GROUP BY runner_id;
+
+
+/********************************************/
+/**********COMENTARIO DANI*******************/
+/********************************************/
+/* Resultado parcialmente incorrecto, has estado muy muy cerquita, te explico. El resultset 
+que nos devuelve tu consulta no calcula bien la velocidad media, el resultado es demasiado
+bajo, ¿donde está el problema?, pues bien en la línea ..."ISNULL(CAST(AVG(distance/duration)AS decimal(4,2)), 0)  velocidad_avg"
+calculamos la velocidad media dividiendo la distancia entre la duración, sin embargo, la duración está 
+expresada en minutos mientras que la distancia en Kilómetros, primero deberíamos convertir la duración
+a horas para posteriormente hacer la división. Por otra parte me ha gustado el enfoque de usar CTE,
+la legibilidad es cada día mas impecable, Enhorabuena Elisa! y también destacar que has tenido en cuenta
+el control de NULLS. Te animo a que le des una vueltita (te chivo que no es difícil) y me pongas la corrección 
+abajo de este comentario!. Ánimo Elisa, a por todas!*/
