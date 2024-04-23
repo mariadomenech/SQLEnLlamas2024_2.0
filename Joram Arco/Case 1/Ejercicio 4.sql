80bee2e99b8d77d8583f203c9beefcf98519fb2a
JuanpeNavío <156901383+juanpedronavio@users.noreply.github.com>
2024-04-23 08:51:30 +0200

    Comentarios Juanpe 1.4

--- Joram Arco/Case 1/Ejercicio 4.sql
+++ Joram Arco/Case 1/Ejercicio 4.sql
@@ -17,3 +17,8 @@ FROM(
 ) AS CONSULTA
 WHERE ranking = 1;
 /* También se podría hacer un select TOP 1 (ordenando por time_ordered desc) para sacar solo la primera fila, sin necesidad de hacer una subselect */
+/*COMENTARIOS JUANPE:
+TODO CORRECTO Y RESPECTO A TU COMENTARIO NO. Bien por no haber usado el top y solo haberlo comentado si no hubiera estado mal el codigo aunque bien la solución.
+El motivo es que buscamos que el código siempre sea generico, que sirva para los datos actuales e incluso si los datos fueran otros, en este caso no hay empates,
+pero si los hubiera un top 1 no te sacaría los dos que hubieran empatado, SALVO que aparte del TOP 1 usaras WITH TIES, si no lo conces te animo a buscarlo, sirve
+para solventar el problema de los empates en el caso de usar TOP 1*/
