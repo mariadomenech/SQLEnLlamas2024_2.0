3e677792cb2872d7059b44694a79a44abcc53f9c
Maria <61223904+mariadomenech@users.noreply.github.com>
2024-04-23 09:32:37 +0200

    Corrección Caso01_reto4.sql

--- Jose Montero/Caso 01/Caso01_reto4.sql
+++ Jose Montero/Caso 01/Caso01_reto4.sql
@@ -12,3 +12,19 @@ where ss.Num_Ventas=(select top 1 count(s.product_id)
 					from case01.sales s
 					group by s.product_id
 					order by 1 desc)
+
+/*********************************************************/
+/***************** COMENTARIO MARÍA *********************/
+/*********************************************************/
+
+/* 
+
+Resultado OK!
+
+Como consejo: El top 1 solo te va a devolver un único resultado o línea, ¿qué ocurre si existe empate entre dos productos en el número de veces que se han pedido? 
+Haría uso de la función RANK(), por ejemplo, que si para una misma partición, dos valores son iguales al ordenarlos, tienen el mismo rango o número y 
+nos permite sacar más de un producto en caso de empate.
+
+Legibilidad: Ok.
+
+*/
