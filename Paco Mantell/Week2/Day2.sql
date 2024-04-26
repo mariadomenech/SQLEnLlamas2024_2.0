1b5f8bea32140386e536f3bc33d111910654f26f
pacomantell <109753909+pacomantell@users.noreply.github.com>
2024-04-26 13:49:13 +0200

    Update Day2.sql

--- Paco Mantell/Week2/Day2.sql
+++ Paco Mantell/Week2/Day2.sql
@@ -71,3 +71,72 @@ es mala, pero podría ser mejor, intenta indexar los atributos y campos, eso seg
 te ayudará un montón. En cuanto tengas la consulta corregida plásmamela abajo de este
 comentario. Ánimo Paco, sé que puedes, tu consulta muestra originalidad y habilidad
 para resolver problemas! */
+
+WITH CTE_ORDERS AS(
+	SELECT 
+		order_id,
+		pizza_id,
+		NULLIF(NULLIF(exclusions, ''), 'null') exclusions,
+		NULLIF(NULLIF(extras, ''), 'null') extras
+	FROM case02.customer_orders
+),  CTE_CLEAN_RUNNER AS (
+	SELECT 
+		order_id,
+		runner_id,
+		NULLIF(NULLIF(cancellation, ''), 'null') cancellation
+	FROM case02.runner_orders
+), CTE_MOD_SUCCESS_PIZZAS AS (
+	SELECT 
+		A.runner_id,
+		COUNT(C.pizza_id) mod_success_pizzas
+	FROM case02.runners A
+	LEFT JOIN CTE_CLEAN_RUNNER B
+		ON A.runner_id=B.runner_id
+	LEFT JOIN CTE_ORDERS C
+		ON B.order_id=C.order_id
+	WHERE B.cancellation IS NULL
+		AND C.exclusions IS NOT NULL
+		AND C.extras IS NOT NULL
+	GROUP BY A.runner_id
+), CTE_SUCCESS_ORDERS AS (
+	SELECT 
+		A.runner_id,
+		COUNT(DISTINCT B.order_id) success_orders
+	FROM case02.runners A
+	LEFT JOIN CTE_CLEAN_RUNNER B
+		ON A.runner_id = B.runner_id
+	WHERE B.cancellation IS NULL
+	GROUP BY A.runner_id
+), CTE_SUCCESS_PIZZAS AS (
+	SELECT 
+		A.runner_id,
+		COUNT(C.pizza_id) success_pizzas
+	FROM case02.runners A
+	LEFT JOIN CTE_CLEAN_RUNNER B
+		ON A.runner_id=B.runner_id
+	LEFT JOIN CTE_ORDERS C
+		ON B.order_id=C.order_id
+	WHERE B.cancellation IS NULL
+	GROUP BY A.runner_id
+), CTE_TOT_ORDERS AS(
+	SELECT 
+		A.runner_id,
+		COUNT(DISTINCT B.order_id) tot_orders
+	FROM case02.runners A
+	LEFT JOIN CTE_CLEAN_RUNNER B
+		ON A.runner_id = B.runner_id
+	GROUP BY A.runner_id
+)
+SELECT 
+	A.runner_id,
+	A.success_orders,
+	CONCAT(CONVERT(FLOAT,ISNULL((A.success_orders * 1.0 / NULLIF(B.tot_orders, 0)),0.0) * 100),'%') success_order_rate,
+	C.success_pizzas,
+	CONCAT(CONVERT(FLOAT,ISNULL((D.mod_success_pizzas * 1.0 / NULLIF(C.success_pizzas,0)),0.0) * 100),'%') success_pizza_rate
+FROM CTE_SUCCESS_ORDERS A
+LEFT JOIN CTE_TOT_ORDERS B
+	ON A.runner_id=B.runner_id
+LEFT JOIN CTE_SUCCESS_PIZZAS C
+	ON A.runner_id=C.runner_id
+LEFT JOIN CTE_MOD_SUCCESS_PIZZAS D
+	ON A.runner_id=D.runner_id;
