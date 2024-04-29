3955bb52f82b14e1227756553e2275f1f6e3ba27
ismaelnavarro <166735719+ismaelnavarro@users.noreply.github.com>
2024-04-29 15:23:11 +0200

    Create Ejercicio2_4.sql

---
+++ Ismael Navarro/WEEK 2/Ejercicio2_4.sql
@@ -0,0 +1,16 @@
+-- Primero, creamos una tabla con todos los toppings de todas las pizzas
+WITH AllToppings AS (
+    SELECT prs.pizza_id, pt.topping_name 
+    FROM case02.pizza_recipes_split prs 
+-- La función CROSS APPLY con VALUES permite tratar cada topping como una fila separada de la consulta, lo que facilita el conteo de cuántas veces se utiliza cada topping
+    CROSS APPLY (VALUES (topping_1), (topping_2), (topping_3), (topping_4), (topping_5), (topping_6), (topping_7), (topping_8)) AS t(topping_id)
+    JOIN case02.pizza_toppings pt ON t.topping_id = pt.topping_id
+)
+-- Luego, realizamos la consulta en la tabla AllToppings
+SELECT topping_name
+    ,COALESCE(COUNT(*),0) AS times_used
+FROM AllToppings
+GROUP BY topping_name
+ORDER BY times_used DESC;
+
+select* from case02.pizza_recipes_split
