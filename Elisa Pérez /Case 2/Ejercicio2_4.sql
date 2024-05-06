98f3c7d87b589886c660add0e36ad4a1a6dfdfd9
DanIngeneer <156660817+DaniNgeneer@users.noreply.github.com>
2024-05-06 13:06:27 +0200

    Update Ejercicio2_4.sql

--- Elisa Pérez /Case 2/Ejercicio2_4.sql
+++ Elisa Pérez /Case 2/Ejercicio2_4.sql
@@ -10,3 +10,15 @@ ON	PT.topping_id = PR.topping_1
 	OR PT.topping_id = PR.topping_7
 	OR PT.topping_id = PR.topping_8
 GROUP BY PT.topping_name
+
+
+/********************************************/
+/*************COMENTARIO DANI****************/
+/********************************************/
+/* Resultado correcto, no hay malabares, simple, sencillo y eficaz.
+Enhorabuena Elisa!. Como detallitos a destacar, te animo a enfocar un
+JOIN mas concreto para evitar posibles problemas de cómputo si las
+tablas contienen muchos datos, por otra parte te recomiendo usar un 
+ORDER BY por ejemplo, para que la consulta nos devuelva los datos
+ordenados de menor a mayor o viceversa!. Sigue así Elisa! Las querys
+te van haciendo cada día mas poderosa!*/
