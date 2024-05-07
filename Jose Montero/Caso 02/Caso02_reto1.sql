a93bc056bbd745bcb2346b766932215c9556adc4
Pablo Nieto <153496982+panieto@users.noreply.github.com>
2024-05-07 19:31:00 +0200

    Comentarios Pablo

--- Jose Montero/Caso 02/Caso02_reto1.sql
+++ Jose Montero/Caso 02/Caso02_reto1.sql
@@ -15,3 +15,33 @@ from #pedidos p
 right join case02.runners r
 	on r.runner_id=p.runner_id
 group by r.runner_id
+
+/*
+Corrección Pablo: Todo prácticamente correcto.
+
+Resultado: OK. Obtienes justo lo que se pedía.
+Código: OK, si no tenemos en cuenta que falta el código para crear la tabla temporal. Una opción más cómoda, al no necesitar un create y drop 
+de la tabla temporal, sería el uso de CTEs, en este caso tendríamos:
+*/
+
+WITH pedidos AS (
+select
+	a.order_id
+	, a.runner_id
+	, coalesce(cast(left(a.distance, patindex('%[^0-9-.]%', a.distance+' ')-1) as float),0) as distancia
+	, coalesce(cast(left(a.duration, patindex('%[^0-9-.]%', a.duration+' ')-1) as float),0) as duracion	
+from case02.runner_orders a
+group by a.order_id, a.runner_id, a.distance, a.duration
+)
+select
+	r.runner_id
+	, coalesce(sum(p.distancia),0) as Distancia_total
+	, round(coalesce((avg(case when p.duracion>0 then p.distancia/(p.duracion/60) else null end)),0),2) as KM_H
+from pedidos p
+right join case02.runners r
+	on r.runner_id=p.runner_id
+group by r.runner_id;
+
+Legibilidad: OK. ¡Mucho mejor estos últimos ejercicios con todo bien tabulado!
+Además, has limpiado perfectamente los datos iniciales. ¡Enhorabuena!
+*/
