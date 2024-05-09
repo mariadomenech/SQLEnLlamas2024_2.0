b0dd2abebd6738e9320f79737cfef02596573652
Joram <166694672+joramarcoi@users.noreply.github.com>
2024-05-09 12:02:21 +0200

    Create Ejercicio 2.sql

---
+++ Joram Arco/Case 3/Ejercicio 2.sql
@@ -0,0 +1,48 @@
+/*
+En esta primera tabla obtenemos los 3 primeros caracteres del mes de la fecha, además del propio año de la fecha. 
+También realizamos el conteo de los depósitos, las compras y las retiradas
+*/
+WITH calculos_clientes AS(
+	SELECT
+		customer_id
+		,CONVERT(CHAR(3), DATENAME(MONTH, txn_date), 0) AS mes
+		,YEAR(txn_date) AS anio
+		,COUNT(CASE WHEN txn_type = 'deposit' THEN txn_type END) AS num_depositos
+		,COUNT(CASE WHEN txn_type = 'purchase' THEN txn_type END) AS num_compras
+		,COUNT(CASE WHEN txn_type = 'withdrawal' THEN txn_type END) AS num_retiros
+	FROM case03.customer_transactions
+	GROUP BY customer_id
+		,txn_type
+		,txn_date
+),
+
+/*
+En esta segunda tabla hacemos el conteo de los clientes, así como las sumas de los conteos que hemos hecho
+en la tabla anterior, para usarlas luego como filtro en la siguiente.
+*/
+suma_calculos AS(
+	SELECT
+		COUNT(DISTINCT customer_id) AS clientes
+		,anio
+		,mes
+		,SUM(num_depositos) AS depositos
+		,SUM(num_compras) AS compras
+		,SUM(num_retiros) AS retiros
+	FROM calculos_clientes
+	GROUP BY customer_id
+		,anio
+		,mes
+)
+
+/*
+Aquí calculamos la suma de los clientes por cada año y mes, teniendo en cuenta los depósitos, compras y retiradas.
+También se podría haber hecho un HAVING en la tabla anterior de la forma HAVING (SUM(num_depositos) > 1 AND SUM(num_compras) > 1) OR SUM(num_retiros) > 1
+*/
+SELECT
+	anio
+	,mes
+	,SUM(clientes)
+FROM suma_calculos
+WHERE (depositos > 1 AND compras > 1) OR retiros > 1
+GROUP BY anio
+	,mes
