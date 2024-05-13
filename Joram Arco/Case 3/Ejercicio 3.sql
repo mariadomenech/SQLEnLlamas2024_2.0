115b5b89966be24b27ff878bd25eca48b9269faf
Joram <166694672+joramarcoi@users.noreply.github.com>
2024-05-13 13:06:03 +0200

    Update

--- Joram Arco/Case 3/Ejercicio 3.sql
+++ Joram Arco/Case 3/Ejercicio 3.sql
@@ -10,23 +10,30 @@ BEGIN
 			customer_id
 		FROM case03.customer_transactions
 		WHERE customer_id = @customerID)
-		BEGIN --Si existe realizamos la suma de las compras del mismo para el mes indicado
-			SELECT 
+	BEGIN
+		IF @mes < 1 OR @mes > 12 --Comprobamos que el mes introducido está en el rango deseado, si no mostramos un mensaje de error
+			SET @OutputMensaje = 'No existe el mes elegido. Por favor, introduzca un número válido (entre 1 y 12).'
+		ELSE
+			BEGIN
+				SELECT 
 				@totalCompras = ISNULL(SUM(txn_amount),0) 
-			FROM case03.customer_transactions
-			WHERE customer_id = @customerID
-				AND MONTH(txn_date) = @mes
-				AND txn_type = 'purchase'
-			SET @OutputMensaje = 'El cliente ' + CAST(@customerID AS VARCHAR(5)) + ' se ha gastado un total de ' + CAST(@totalCompras AS VARCHAR(10)) + '€ en el mes de ' + DATENAME(MONTH, DATEFROMPARTS(YEAR(GETDATE()), @mes, 1))
-			SELECT @OutputMensaje AS Mensaje --Mostramos el mensaje arriba indicado
-		END
+				FROM case03.customer_transactions
+				WHERE customer_id = @customerID
+					AND MONTH(txn_date) = @mes
+					AND txn_type = 'purchase'
+				SET @OutputMensaje = 'El cliente ' + CAST(@customerID AS VARCHAR(5)) + ' se ha gastado un total de ' + CAST(@totalCompras AS VARCHAR(10)) + '€ en el mes de ' + DATENAME(MONTH, DATEFROMPARTS(YEAR(GETDATE()), @mes, 1))
+			END
+	END	
 	ELSE --Si el cliente no existe mostramos un mensaje indicándolo
-		BEGIN
-			SET @OutputMensaje = 'No existe el usuario con el id ' + CAST(@customerID AS VARCHAR(5))
-			SELECT @OutputMensaje AS Mensaje
-			-- También tendríamos la opción de mostrarlo por pantalla al compilar con la opción PRINT @OutputMensaje
-		END
+		SET @OutputMensaje = 'No existe el usuario con el id ' + CAST(@customerID AS VARCHAR(5))
+
+	SELECT @OutputMensaje AS Mensaje
+	-- También tendríamos la opción de mostrarlo por pantalla al compilar con la opción PRINT @OutputMensaje
 END;
 
 EXEC TotalComprasClienteMes @customerID = 123, @mes = 3;
-EXCE TotalComprasClientesMes @customerID = 999, @mes = 3;
+EXEC TotalComprasClienteMes @customerID = 999, @mes = 3;
+EXEC TotalComprasClienteMes @customerID = 123, @mes = 78;
+
+
+DROP PROCEDURE TotalComprasClienteMes;
