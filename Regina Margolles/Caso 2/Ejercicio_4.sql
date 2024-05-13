1141d2015ab38ec44a0a5157285bc77f1971f868
Maria <61223904+mariadomenech@users.noreply.github.com>
2024-05-13 09:15:06 +0200

    Corrección Ejercicio_4.sql

--- Regina Margolles/Caso 2/Ejercicio_4.sql
+++ Regina Margolles/Caso 2/Ejercicio_4.sql
@@ -16,3 +16,19 @@ from
 
 group by id_topping, p.topping_name
 order by veces_usado desc;
+
+
+/*********************************************************/
+/***************** COMENTARIO MARÍA  *********************/
+/*********************************************************/
+/*
+
+Resultado correcto! 
+Legibilidad perfecta!
+La idea de este ejercicio era que utilizáseis la funcion UNPIVOT, porque es más genérico y puedes usarlo en otros gestores.
+Perfecto!
+
+Si es verdad, que en la línea 14, haríamos un RIGHT JOIN porque la tabla de toppings es una tabla de dimensiones de la receta. 
+Puede haber más toppings en la tabla de toppings que los comentados en la tabla de recetas.
+
+*/
