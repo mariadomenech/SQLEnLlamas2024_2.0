e647c3b6b48452a52be040b60a72f0cb3a98a943
peter-navarro <166490623+peter-navarro@users.noreply.github.com>
2024-05-15 20:25:40 +0200

    Update challenge_03.sql

--- Pedro Navarro/Case 03/challenge_03.sql
+++ Pedro Navarro/Case 03/challenge_03.sql
@@ -5,19 +5,18 @@ USE SQL_EN_LLAMAS_ALUMNOS;
 GO
 
 -- Creación del procedimiento almacenado
-CREATE OR ALTER PROCEDURE case02.PNG_Ejercicio3_1_Procedimiento1
+CREATE OR ALTER PROCEDURE case02.PNG_Ejercicio3_3_Procedimiento
 	@customer_id INT
 	,@year INT
 	,@month_name VARCHAR(10)
 AS
 BEGIN
 	-- Cálculo del gasto total de compras
-	DECLARE @total_spend NUMERIC(20,2);
+	DECLARE @total_purchase NUMERIC(20,2);
 
-	SELECT @total_spend = SUM(txn_amount)
+	SELECT @total_purchase = SUM(CASE WHEN txn_type LIKE 'purchase' THEN txn_amount ELSE 0 END)
 	FROM SQL_EN_LLAMAS_ALUMNOS.case03.customer_transactions
-	WHERE txn_type LIKE 'purchase'
-		AND customer_id = @customer_id
+	WHERE customer_id = @customer_id
 		AND YEAR(txn_date) = @year
 		AND LOWER(DATENAME(month, txn_date)) LIKE LOWER(@month_name)
 	GROUP BY
@@ -26,7 +25,7 @@ BEGIN
 		,DATENAME(month, txn_date);
 
 	-- Control de resultado nulo
-	IF @total_spend IS NULL
+	IF @total_purchase IS NULL
 		DECLARE @customer_log INT;
 
 		SELECT @customer_log = customer_id
@@ -105,7 +104,7 @@ BEGIN
 
 	ELSE
 		PRINT 'EL CLIENTE ' + CAST(@customer_id AS VARCHAR(10)) + ' SE HA GASTADO UN TOTAL DE ' +
-				CAST(@total_spend AS VARCHAR(23)) + ' EUR EN COMPRAS DE PRODUCTOS EN EL MES DE ' +
+				CAST(@total_purchase AS VARCHAR(23)) + ' EUR EN COMPRAS DE PRODUCTOS EN EL MES DE ' +
 				UPPER(@month_name) + ' DEL AÑO ' + CAST(@year AS VARCHAR(4));
 END;
 GO
@@ -117,7 +116,7 @@ SET @check_year = 2020
 SET @check_month_name = 'marzo'
 
 -- Ejecución del procedimiento almacenado
-EXECUTE case02.PNG_Ejercicio3_1_Procedimiento1 @customer_id = @check_customer_id, @year = @check_year, @month_name = @check_month_name;
+EXECUTE case02.PNG_Ejercicio3_3_Procedimiento @customer_id = @check_customer_id, @year = @check_year, @month_name = @check_month_name;
 GO
 -- Eliminación el procedimiento almacenado
-DROP PROCEDURE case02.PNG_Ejercicio3_1_Procedimiento1;
+DROP PROCEDURE case02.PNG_Ejercicio3_3_Procedimiento;
