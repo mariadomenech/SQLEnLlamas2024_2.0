69e7a3b3cbb495d9fdbffd9db16537aaac7e6344
pablovilla6 <166702152+pablovilla6@users.noreply.github.com>
2024-05-17 10:25:11 +0200

    Create Ejercicio 2.4.sql

---
+++ Pablo Villanueva/Case 2/Ejercicio 2.4.sql
@@ -0,0 +1,11 @@
+SELECT 
+    pt.topping_name,
+    COALESCE(COUNT(*), 0) AS veces_usado
+FROM 
+    case02.pizza_recipes_split prs
+JOIN 
+    case02.pizza_toppings pt ON pt.topping_id IN (prs.topping_1, prs.topping_2, prs.topping_3, prs.topping_4, prs.topping_5, prs.topping_6, prs.topping_7, prs.topping_8)
+GROUP BY 
+    pt.topping_name
+ORDER BY 
+    veces_usado DESC;
