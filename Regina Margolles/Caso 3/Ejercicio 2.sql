72d21b51b7d5a919464f22d083a68f22caa6461b
ReginaMargolles81 <140602073+ReginaMargolles81@users.noreply.github.com>
2024-05-18 17:45:41 +0200

    Update Ejercicio 2.sql

--- Regina Margolles/Caso 3/Ejercicio 2.sql
+++ Regina Margolles/Caso 3/Ejercicio 2.sql
@@ -1,1 +1,27 @@
+Select year, month, count(customer_id) as customer_number
+from
+		(
+			Select
+					year,
+					month,
+					customer_id,
+					COUNT(deposit) as n_deposit,
+					COUNT(purchase) as n_purchase,
+					COUNT(withdrawal) as n_withdrawal
+			from
+					(Select 
+						   datename(year,(txn_date))as year,
+						   datename(month,(txn_date)) as month,
+						   customer_id,
+						   case when (txn_type = 'deposit') then 1  end as deposit,
+						   case when (txn_type = 'purchase') then 1  end as purchase,
+						   case when (txn_type = 'withdrawal') then 1 end as withdrawal
+						   from case03.customer_transactions
+					)as t
 
+			group by year, month, customer_id
+			
+		)as t2
+where (n_deposit > 1 and  n_purchase>1) or (n_withdrawal>1)
+group by year, month
+order by customer_number 
