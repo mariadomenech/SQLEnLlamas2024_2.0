dfeefcf0996e24638929c2abf6dd23b9198b2ceb
JoseLuisMontalvo <166693694+JoseLuisMontalvo@users.noreply.github.com>
2024-05-19 18:41:12 +0200

    query3.sql

---
+++ JoseLuisMontalvo/case03/reto3/query3.sql
@@ -0,0 +1,24 @@
+/* Reto3 , Crear un procedimiento almacenado que al introducir el identificador de cliente y el mes, calcule el total de compras y devuelva el mensaje
+           "El cliente x , ha gastado un total de xxx eur en compras de productos en el mes de marzo"*/
+
+create procedure JLMG_Ejercicio3_3_TotalCompras @cliente int,@mes int,@anio int   
+as
+
+declare @total decimal(18,2)
+declare @resultado varchar(150)
+
+select @total=sum(txn_amount) 
+      from case03.customer_transactions
+	  where customer_id=@cliente and 
+	        year(txn_date)=@anio and
+			month(txn_date)=@mes
+
+if (@total is null or @total=0)
+   begin
+       set @resultado='El cliente '+convert(varchar,@cliente)+' no ha tenido gastos en el mes de '+datename(month,'01-'+convert(varchar,@mes)+'-'+convert(varchar,@anio))+' de '+convert(varchar,@anio)
+   end else
+    begin 
+       set @resultado='El cliente '+convert(varchar,@cliente)+' se ha gastado un total de '+convert(varchar,@total)+' euros en el mes de '+datename(month,'01-'+convert(varchar,@mes)+'-'+convert(varchar,@anio))+' de '+convert(varchar,@anio)
+   end
+
+select @resultado;
