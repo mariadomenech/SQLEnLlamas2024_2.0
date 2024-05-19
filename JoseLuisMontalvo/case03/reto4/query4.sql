a599f3763c7a98d580e08a430f440d48201d24e9
JoseLuisMontalvo <166693694+JoseLuisMontalvo@users.noreply.github.com>
2024-05-19 19:38:08 +0200

    query4.sql

---
+++ JoseLuisMontalvo/case03/reto4/query4.sql
@@ -0,0 +1,108 @@
+/* Reto4 , Crear un procedimiento almacenado que al introducir el identificador de cliente y el mes, calcule el total de compras y devuelva el mensaje
+           "El cliente x , ha gastado un total de xxx eur en compras de productos en el mes de marzo"*/
+
+create procedure JLMG_Ejercicio3_4_TotalMovimientoCliente @tipo_calculo int,@cliente int,@mes int,@anio int   
+as
+/*
+@tipo_calculo
+1 -- Si queremos el balance
+2 -- El total depositado
+3 -- El total de compras
+4 -- El total de retiros
+*/
+
+
+declare @depositos decimal(18,2)
+declare @compras decimal(18,2)
+declare @retiros decimal(18,2)
+declare @total decimal(18,2)
+declare @resultado varchar(150)
+
+
+if @tipo_calculo=1  ---- Tipos 1 significa Balance Deposito - compras - retiros
+   begin
+		 select @compras=isnull(sum(txn_amount),0)
+			  from case03.customer_transactions
+			  where customer_id=@cliente and 
+					year(txn_date)=@anio and
+					month(txn_date)=@mes and
+					txn_type='purchase'
+		 select @depositos=isnull(sum(txn_amount),0) 
+			  from case03.customer_transactions
+			  where customer_id=@cliente and 
+					year(txn_date)=@anio and
+					month(txn_date)=@mes and
+					txn_type='deposit'
+		select @retiros=isnull(sum(txn_amount),0) 
+			  from case03.customer_transactions
+			  where customer_id=@cliente and 
+					year(txn_date)=@anio and
+					month(txn_date)=@mes and
+					txn_type='withdrawal'
+       set @total=@depositos-@compras-@retiros
+	   
+	   set @resultado='El balance del cliente '+convert(varchar,@cliente)+' para el mes de '+datename(month,'01-'+convert(varchar,@mes)+'-'+convert(varchar,@anio))+' de '+convert(varchar,@anio)+' es de '+convert(varchar,@total)+' euros'
+end 
+if @tipo_calculo=2  ---- Tipo 2 depositos
+   begin
+	  select @depositos=isnull(sum(txn_amount),0) 
+			  from case03.customer_transactions
+			  where customer_id=@cliente and 
+					year(txn_date)=@anio and
+					month(txn_date)=@mes and
+					txn_type='deposit'
+
+       set @total=@depositos
+	   if @total=0
+		   begin
+			   set @resultado='El cliente '+convert(varchar,@cliente)+' no ha tenido depositos en el mes de '+datename(month,'01-'+convert(varchar,@mes)+'-'+convert(varchar,@anio))+' de '+convert(varchar,@anio)
+		   end else
+			begin 
+			   set @resultado='El cliente '+convert(varchar,@cliente)+' ha depositado un total de '+convert(varchar,@total)+' euros en el mes de '+datename(month,'01-'+convert(varchar,@mes)+'-'+convert(varchar,@anio))+' de '+convert(varchar,@anio)
+		   end
+	   
+end 
+if @tipo_calculo=3  ---- Tipo 3 compras
+   begin
+	  select @depositos=isnull(sum(txn_amount),0) 
+			  from case03.customer_transactions
+			  where customer_id=@cliente and 
+					year(txn_date)=@anio and
+					month(txn_date)=@mes and
+					txn_type='purchase'
+
+       set @total=@depositos
+	   if @total=0
+		   begin
+			   set @resultado='El cliente '+convert(varchar,@cliente)+' no ha comprado en el mes de '+datename(month,'01-'+convert(varchar,@mes)+'-'+convert(varchar,@anio))+' de '+convert(varchar,@anio)
+		   end else
+			begin 
+			   set @resultado='El cliente '+convert(varchar,@cliente)+' ha comprado un total de '+convert(varchar,@total)+' euros en el mes de '+datename(month,'01-'+convert(varchar,@mes)+'-'+convert(varchar,@anio))+' de '+convert(varchar,@anio)
+		   end
+	   
+end 
+if @tipo_calculo=4  ---- Tipo 4 retiros
+   begin
+	  select @depositos=isnull(sum(txn_amount),0) 
+			  from case03.customer_transactions
+			  where customer_id=@cliente and 
+					year(txn_date)=@anio and
+					month(txn_date)=@mes and
+					txn_type='withdrawal'
+
+       set @total=@depositos
+	   if @total=0
+		   begin
+			   set @resultado='El cliente '+convert(varchar,@cliente)+' no ha retirado en el mes de '+datename(month,'01-'+convert(varchar,@mes)+'-'+convert(varchar,@anio))+' de '+convert(varchar,@anio)
+		   end else
+			begin 
+			   set @resultado='El cliente '+convert(varchar,@cliente)+' ha retirado un total de '+convert(varchar,@total)+' euros en el mes de '+datename(month,'01-'+convert(varchar,@mes)+'-'+convert(varchar,@anio))+' de '+convert(varchar,@anio)
+		   end	   
+end 
+
+if @tipo_calculo<1 or @tipo_calculo>4  --- Si nos pasan un tipo de cálculo no definido devolvemos un mensaje de error
+  begin
+     set @resultado='ERROR: Tipo de cálculo seleccionado no está no disponible'
+  end
+
+select @resultado;
