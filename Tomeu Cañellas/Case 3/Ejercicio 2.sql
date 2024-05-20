e77061d24dad0a399ac1e364a91d2c14248cbb10
AlexArcoCivica <156660711+AlexArcoCivica@users.noreply.github.com>
2024-05-20 07:59:01 +0200

    Comentario Ejercicio 2.sql

--- Tomeu Cañellas/Case 3/Ejercicio 2.sql
+++ Tomeu Cañellas/Case 3/Ejercicio 2.sql
@@ -39,3 +39,52 @@ WHERE (deposits > 1 AND purchases > 1) OR (withdrawals > 1)
 GROUP BY year,
          month
 ;
+
+/*********************************************************/
+/***************** COMENTARIO ALEX *********************/
+/*********************************************************/
+/*
+
+Está perfecto. Sencillo y legible.
+Como curiosidad, para que los meses en vez de salir en número te ponga el mismo formato que la solución de la app puedes
+usar FORMAT(txn_date, 'MMM', 'ES'):
+
+WITH txn_per_month AS (
+    SELECT  customer_id, 
+	    FORMAT(txn_date, 'MMM', 'ES') AS month,
+            YEAR(txn_date) AS year,
+            txn_type,
+            COUNT(*) AS txn_count
+    FROM [SQL_EN_LLAMAS_ALUMNOS].[case03].[customer_transactions]
+    GROUP BY customer_id, 
+			 FORMAT(txn_date, 'MMM', 'ES'),
+             YEAR(txn_date),
+             txn_type
+),
+--En esta tabla temporal obtenemos la suma del conteo anterior dependiendo del tipo de transaccion agrupada por cliente/mes/año
+txn_per_type AS (
+    SELECT  customer_id,
+            month,
+            year,
+            SUM(CASE WHEN txn_type = 'withdrawal' THEN txn_count 
+                     ELSE 0 END) AS withdrawals,
+            SUM(CASE WHEN txn_type = 'purchase' THEN txn_count 
+                     ELSE 0 END) AS purchases,
+	    SUM(CASE WHEN txn_type = 'deposit' THEN txn_count 
+                     ELSE 0 END) AS deposits
+    FROM txn_per_month
+    GROUP BY customer_id,
+             month,
+             year
+)
+--Obtenemos el número de clientes que cumplen las condiciones del enunciado en la clausula WHERE y agrupamos por mes/año
+SELECT  year,
+        month,
+        COUNT(*) AS client_count
+FROM txn_per_type
+WHERE (deposits > 1 AND purchases > 1) OR (withdrawals > 1)
+GROUP BY year,
+         month
+;
+
+*/
