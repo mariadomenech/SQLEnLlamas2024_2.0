8ece56b9a8e9bd2e88840b13cf0505d34cabf664
pablovilla6 <166702152+pablovilla6@users.noreply.github.com>
2024-05-21 08:07:36 +0200

    Update Ejercicio 2.5.sql

--- Pablo Villanueva/Case 2/Ejercicio 2.5.sql
+++ Pablo Villanueva/Case 2/Ejercicio 2.5.sql
@@ -6,11 +6,7 @@ WITH order_status AS (
         CASE
             WHEN cancellation = '' OR cancellation = 'null' OR cancellation IS NULL THEN 1
             ELSE 0
-        END AS orders_done,
-        CASE
-            WHEN cancellation <> '' AND cancellation <> 'null' AND cancellation IS NOT NULL THEN 1
-            ELSE 0
-        END AS orders_cancelled
+        END AS orders_done
     FROM case02.runner_orders
 ),
 
@@ -24,25 +20,73 @@ pizzas_delivered AS (
         ON os.order_id = co.order_id
     WHERE os.orders_done = 1
     GROUP BY os.runner_id
+),
+
+-- CTE para contar los ingredientes usados en pizzas entregadas con éxito
+ingredient_usage AS (
+    SELECT
+        pt.topping_id,
+        pt.topping_name,
+        COUNT(*) AS veces_usado
+    FROM order_status os
+    JOIN case02.customer_orders co
+        ON os.order_id = co.order_id
+    JOIN case02.pizza_recipes_split prs
+        ON prs.pizza_id = co.pizza_id
+    JOIN case02.pizza_toppings pt
+        ON pt.topping_id IN (prs.topping_1, prs.topping_2, prs.topping_3, prs.topping_4, prs.topping_5, prs.topping_6, prs.topping_7, prs.topping_8)
+    WHERE os.orders_done = 1
+    GROUP BY pt.topping_id, pt.topping_name
+),
+
+-- CTE para contar los ingredientes adicionales
+ing_extras AS (
+    SELECT 
+        pt.topping_id,
+        pt.topping_name,
+        COUNT(1) AS veces_extras
+    FROM case02.customer_orders co
+    CROSS APPLY STRING_SPLIT(co.extras, ',') AS extra
+    JOIN case02.pizza_toppings pt ON TRY_CAST(extra.value AS INT) = pt.topping_id
+    WHERE co.order_id NOT IN (
+        SELECT order_id FROM case02.runner_orders WHERE UPPER(cancellation) LIKE '%CANCELLATION%'
+    )
+    GROUP BY pt.topping_id, pt.topping_name
+),
+
+-- CTE para contar las exclusiones de ingredientes
+ing_exclusions AS (
+    SELECT 
+        pt.topping_id,
+        pt.topping_name,
+        COUNT(1) AS veces_exclusions
+    FROM case02.customer_orders co
+    CROSS APPLY STRING_SPLIT(co.exclusions, ',') AS exclusion
+    JOIN case02.pizza_toppings pt ON TRY_CAST(exclusion.value AS INT) = pt.topping_id
+    WHERE co.order_id NOT IN (
+        SELECT order_id FROM case02.runner_orders WHERE UPPER(cancellation) LIKE '%CANCELLATION%'
+    )
+    GROUP BY pt.topping_id, pt.topping_name
+),
+
+-- CTE para sumar los ingredientes usados, extras y restar las exclusiones
+ing_total AS (
+    SELECT 
+        pt.topping_id,
+        pt.topping_name,
+        COALESCE(iu.veces_usado, 0) + COALESCE(ie.veces_extras, 0) - COALESCE(ix.veces_exclusions, 0) AS num_ingredientes_gen
+    FROM case02.pizza_toppings pt
+    LEFT JOIN ingredient_usage iu ON pt.topping_id = iu.topping_id
+    LEFT JOIN ing_extras ie ON pt.topping_id = ie.topping_id
+    LEFT JOIN ing_exclusions ix ON pt.topping_id = ix.topping_id
 )
 
 -- Seleccionamos los resultados finales
 SELECT
-	pt.topping_id,
-	Count(*) as topping_veces_usados,
-	topping_name
-FROM
-    order_status os
-LEFT JOIN
-    pizzas_delivered pd
-    ON os.runner_id = pd.runner_id
-JOIN case02.customer_orders co 
-    ON os.order_id = co.order_id
-JOIN [case02].[pizza_recipes_split] pres
-	ON pres.pizza_id= co.pizza_id
-JOIN 
-    case02.pizza_toppings pt ON pt.topping_id IN (pres.topping_1, pres.topping_2, pres.topping_3, pres.topping_4, pres.topping_5, pres.topping_6, pres.topping_7, pres.topping_8)
-GROUP BY
-    pt.topping_id,topping_name
-ORDER BY
-    topping_veces_usados DESC;
+    RANK() OVER (ORDER BY num_ingredientes_gen DESC) AS ranking,
+    num_ingredientes_gen,
+    STRING_AGG(topping_name, ', ') AS ingredientes
+FROM ing_total
+GROUP BY num_ingredientes_gen
+ORDER BY ranking;
+
