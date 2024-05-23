2c7fd18a5f3499544115dfffd95e5bfd6e862683
Joram <166694672+joramarcoi@users.noreply.github.com>
2024-05-23 14:34:34 +0200

    Update

--- Joram Arco/Case 4/Ejercicio 2.sql
+++ Joram Arco/Case 4/Ejercicio 2.sql
@@ -2,7 +2,7 @@
 WITH lista_productos_mas_3_transacciones AS (
     SELECT 
         txn_id
-        ,STRING_AGG(product_name, ' - ') AS lista_productos
+        ,STRING_AGG('- '+product_name, CHAR(10)) WITHIN GROUP (ORDER BY product_name) AS lista_productos
     FROM case04.sales sales
     JOIN case04.product_details product_details 
 		ON sales.prod_id = product_details.product_id
@@ -23,7 +23,7 @@ veces_repetida AS (
 ordenamos por ranking para obtener el mayor valor (por si en algún caso se repiten obtener los duplicados) */
 SELECT 
 	num_veces_repetida
-	,CONCAT('La combinación más repetida es: ', desc_combinacion)
+	,CONCAT('La combinación más repetida es: ', CHAR(10), desc_combinacion)
 FROM (
 	SELECT 
 		RANK() OVER (ORDER BY num_veces_repetida DESC) as ranking
