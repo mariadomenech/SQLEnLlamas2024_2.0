f5b09e0f937e80cced418f168d7c22438c6511b0
tomeucc <166701083+tomeucc@users.noreply.github.com>
2024-05-27 20:38:00 +0200

    Ejercicio 3.sql

--- Tomeu Cañellas/Case 4/Ejercicio 3.sql
+++ Tomeu Cañellas/Case 4/Ejercicio 3.sql
@@ -1,14 +1,14 @@
 --CASO 4: Ejercicio 3
 --Cruzamos la tabla de product_prices con product_hierarchy por sus respectivos id
 SELECT 	pri.product_id,
-		pri.price,
-		CONCAT(hie.level_text, ' ', hier.level_text, ' - ', hiera.level_text) AS product_name,
-		hiera.id AS category_id,
-		hier.id AS segment_id,
-		hie.id AS style_id,
-		hiera.level_text AS category_name,
-		hier.level_text AS segment_name,
-		hie.level_text AS style_name
+	pri.price,
+	CONCAT(hie.level_text, ' ', hier.level_text, ' - ', hiera.level_text) AS product_name,
+	hiera.id AS category_id,
+	hier.id AS segment_id,
+	hie.id AS style_id,
+	hiera.level_text AS category_name,
+	hier.level_text AS segment_name,
+	hie.level_text AS style_name
 FROM [SQL_EN_LLAMAS_ALUMNOS].[case04].[product_prices] pri
 INNER JOIN [SQL_EN_LLAMAS_ALUMNOS].[case04].[product_hierarchy] hie
 	ON pri.id = hie.id
