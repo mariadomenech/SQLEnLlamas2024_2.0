23e8b50e7b7c3c69fd5710c53960a4b6f924fdc4
AnaBoschF <167062264+AnaBoschF@users.noreply.github.com>
2024-05-28 18:30:41 +0200

    Reto 3.sql

--- Ana Bosch/Caso 4/Reto 3.sql
+++ Ana Bosch/Caso 4/Reto 3.sql
@@ -20,3 +20,57 @@ JOIN case04.product_hierarchy STY
 -- Join con la tabla prices para obtener el precio y el id_prodcut
 JOIN case04.product_prices prices
 	ON STY.id = prices.id
+
+---------------------------------------------------------------------- IMPORTANT!------------------------------------------------------------------------
+
+/* SOLUCIÓN 2.0
+Junto con Joram, nos hemos dado cuenta que estabamos frente a un claro ejemplo de recursividad por lo tanto hemos considerado buena solución 
+el uso de CTE como caso base (recordando algoritmos recursivos) del cual partamos para recorrer recursivamente los nodos padres */
+
+-- CTE del caso base (categoria)
+WITH CTE_CATEGORIA
+AS (
+	SELECT CAT.id AS CAT_id
+		,CAT.level_text AS CAT_level_text
+	FROM case04.product_hierarchy CAT
+	WHERE level_name = 'Category'
+),
+
+-- CTE para a partir del caso base recurrentemente recorrer los ids padres para obtener el segmento
+CTE_SEGMENTO
+AS (
+	SELECT CAT_id
+		,SEG.id AS SEG_id
+		,CAT_level_text
+		,SEG.level_text AS SEG_level_text
+	FROM CTE_CATEGORIA CAT
+	JOIN case04.product_hierarchy SEG
+		ON CAT.CAT_id = SEG.parent_id
+	WHERE level_name = 'Segment'
+),
+
+-- CTE para recurrentemente recorrer los ids padres para obtener el estilo
+CTE_STYLE
+AS (
+	SELECT CONCAT(STY.level_text, ' ', SEG_level_text, ' - ', CAT_level_text) AS product_name
+		,CAT_id AS category_id
+		,SEG_id AS segment_id
+		,STY.id AS style_id
+		,CAT_level_text AS category_name
+		,SEG_level_text AS segment_name
+		,STY.level_text AS style_name
+	FROM CTE_SEGMENTO SEG
+	JOIN case04.product_hierarchy STY
+		ON SEG.SEG_id = STY.parent_id
+	WHERE level_name = 'Style'
+)
+
+-- Consulta final para cruzar por product_id y price y obtener la tabla en el formato indicado
+SELECT product_id
+	,price
+	,STY.*
+FROM CTE_STYLE STY
+JOIN case04.product_prices prices
+	ON STY.style_id = prices.id
+
+	
