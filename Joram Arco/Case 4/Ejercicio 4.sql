ec32c7635eebda5dfb64a12321349a6fd9965187
Joram <166694672+joramarcoi@users.noreply.github.com>
2024-05-29 14:16:35 +0200

    Update

--- Joram Arco/Case 4/Ejercicio 4.sql
+++ Joram Arco/Case 4/Ejercicio 4.sql
@@ -1,17 +1,23 @@
---Creamos una primera tabla temporal donde calculamos los ingresos por transacción
+/* Creamos una primera tabla temporal donde calculamos los ingresos brutos y descuento por transacción,
+además tenemos en cuenta que se deben quitar los registros duplicados para evitar que el resultado nos salga mayor
+debido a estos duplicados
+P.D: Me costó darle un rato la lata a María para que me indicase al final esta parte de los duplicados y tenerlo
+en cuenta para que no me bailasen los números :) */
 WITH ingresos AS (
 SELECT 
 	txn_id
 	,SUM(price * qty) AS ingresos_brutos
 	,SUM(price * discount * 0.01 * qty) AS descuento
-FROM case04.sales
+FROM (
+	SELECT DISTINCT  *
+	FROM case04.sales
+) consulta
 GROUP BY txn_id
 ),
 
 /* Después generamos otra tabla temporal donde creamos una variable tipo percentil para indicar si es continuo 
-o discreto y, además, generamos los percentiles 25, 50 y 75 (tanto continuos como discretos) de los ingresos de 
-la tabla anterior y los unimos para obtenerlos en una misma tabla
-*/
+o discreto y, además, generamos los percentiles 25, 50 y 75 (tanto continuos como discretos) de los ingresos brutos
+menos los descuentos de la tabla anterior y los unimos para obtenerlos en una misma tabla */
 continuo_discreto AS (
 SELECT
 	'CONTINUO' AS tipo_percentil
