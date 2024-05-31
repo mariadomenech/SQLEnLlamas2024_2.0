17fe9bd8d3060cfec105154ad3b67aab85575339
sdf007 <167197927+sdf007@users.noreply.github.com>
2024-05-31 08:57:22 +0200

    Update Ejercicio4.sql

--- Sergio Díaz/CASO3/Ejercicio4.sql
+++ Sergio Díaz/CASO3/Ejercicio4.sql
@@ -1,12 +1,15 @@
+--ALERTA! SE ENCONTRARON ERRORES AL ANALIZAR EL CODIGO SQL!
 /* Alumno: Sergio Díaz */
 /* CASO 3 - Ejercicio 4: Evolución del procedure para elegir el tipo de cálculo */
+
+
 
 CREATE PROCEDURE SDF_CASO3_RETO4 @Cliente INT
 	,@Mes VARCHAR(10) -- mes en formato yyyyMM
 	,@Calculo INT --ESTABLECEMOS LOS SIGUIENTES VALORES: 1 BALANCE, 2 DEPOSITOS, 3 COMPRAS, 4 RETIROS
 AS
 BEGIN
-	IF @Calculo = 1 --BALANCE
+	IF @Calculo = 1
 	BEGIN
 		WITH TRANSACCIONES_CLIENTE
 		AS (
@@ -36,13 +39,13 @@ BEGIN
 				,DATENAME(month, DATEFROMPARTS(LEFT(@Mes, 4), RIGHT(@Mes, 2), 1))
 				,' es de '
 				,(
-					SELECT depositos - compras - retiros
+					SELECT ISNULL(depositos - compras - retiros, 0)
 					FROM TRANSACCIONES_CLIENTE
 					)
 				,' Euros'
 				) AS MENSAJE
 	END
-	ELSE IF @Calculo = 2 -- TOTAL DEPOSITOS
+	ELSE IF @Calculo = 2
 	BEGIN
 		SELECT CONCAT (
 				'La cantidad de depositos del cliente '
@@ -60,7 +63,7 @@ BEGIN
 				,' Euros'
 				) AS MENSAJE
 	END
-	ELSE IF @Calculo = 3 -- TOTAL DE COMPRAS
+	ELSE IF @Calculo = 3
 	BEGIN
 		SELECT CONCAT (
 				'La cantidad de compras del cliente '
@@ -78,7 +81,7 @@ BEGIN
 				,' Euros'
 				) AS MENSAJE
 	END
-	ELSE IF @Calculo = 4 -- TOTAL DE RETIROS
+	ELSE IF @Calculo = 4
 	BEGIN
 		SELECT CONCAT (
 				'La cantidad de retiros del cliente '
@@ -96,7 +99,7 @@ BEGIN
 				,' Euros'
 				) AS MENSAJE
 	END
-	ELSE -- OTRO VALOR
+	ELSE
 	BEGIN
 		SELECT ('No se ha introducido un valor de cálculo válido. Por favor introduzca un valor entre el 1 y el 4') AS MENSAJE
 	END
@@ -104,13 +107,19 @@ END
 
 -----------------------------------------------------------------
 --EJECUCIONES DEL PROCEDURE
-EXEC SDF_CASO3_RETO4 28, '202004', 1;
-EXEC SDF_CASO3_RETO4 1, '202001', 2;
+EXEC SDF_CASO3_RETO4 28
+	,'202004'
+	,1;
 
+EXEC SDF_CASO3_RETO4 1
+	,'202001'
+	,2;
+	
+	
 /*
 
 COMENTARIO: Dado que queda poco tiempo, dejo sin hacer el control de errores. Creo que me llevaría un tiempo que tampoco aportaría mucho
 valor a la solución de este reto puesto que sería añadir más de lo mismo (lógica de IF ELSE para controlar todas las casuísticas).
 No obstante, si me queda tiempo lo actualizaré :)
 
-/*
+/**/
