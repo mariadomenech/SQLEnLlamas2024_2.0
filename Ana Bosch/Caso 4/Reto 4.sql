0560c02a79d9cc3cd1a4c4d57dffd8acd446c62e
AnaBoschF <167062264+AnaBoschF@users.noreply.github.com>
2024-06-01 14:10:21 +0200

    Reto 4.sql

--- Ana Bosch/Caso 4/Reto 4.sql
+++ Ana Bosch/Caso 4/Reto 4.sql
@@ -1,6 +1,6 @@
 -- RETO 4: Cuáles son los valores (importe) de los percentiles 25, 50 y 75 para los ingresos por transacción.
 
--- Tabla temporal para obtener el precio final de cada transacción, teniendo en cuenta el descuento y cazntidad
+-- Tabla temporal para obtener el precio final de cada transacción, teniendo en cuenta el descuento y cantidad
 WITH CTE_PRECIO_TRANSACCION
 AS (
 	SELECT txn_id
@@ -15,18 +15,18 @@ AS (
 -- Se une el calculo de los percentiles continuos con los percentiles discontinuos y se ordena por tipo percentil ascendentemente
 SELECT DISTINCT 
 	'CONTINUO' AS tipo_percentil
-	,PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY preciofinal) OVER () as pc_25
-	,PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY preciofinal) OVER () as pc_50
-	,PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY preciofinal) OVER () as pc_75
+	,PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY preciofinal) OVER () AS pc_25
+	,PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY preciofinal) OVER () AS pc_50
+	,PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY preciofinal) OVER () AS pc_75
 FROM CTE_PRECIO_TRANSACCION
 
 UNION
 
 SELECT DISTINCT 
 	'DISCONTINUO' AS tipo_percentil
-	,PERCENTILE_DISC(0.25) WITHIN GROUP (ORDER BY preciofinal) OVER () as pc_25
-	,PERCENTILE_DISC(0.5) WITHIN GROUP (ORDER BY preciofinal) OVER () as pc_50
-	,PERCENTILE_DISC(0.75) WITHIN GROUP (ORDER BY preciofinal) OVER () as pc_75
+	,PERCENTILE_DISC(0.25) WITHIN GROUP (ORDER BY preciofinal) OVER () AS pc_25
+	,PERCENTILE_DISC(0.5) WITHIN GROUP (ORDER BY preciofinal) OVER () AS pc_50
+	,PERCENTILE_DISC(0.75) WITHIN GROUP (ORDER BY preciofinal) OVER () AS pc_75
 FROM CTE_PRECIO_TRANSACCION
 
 ORDER BY 1 ASC
