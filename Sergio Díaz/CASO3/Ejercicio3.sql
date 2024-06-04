4f9f4c96c77ba98d92ebc2a127b88166855cb4a0
sdf007 <167197927+sdf007@users.noreply.github.com>
2024-06-04 09:11:37 +0200

    Update Ejercicio3.sql
    Se añade chequeo de cliente y mes.

--- Sergio Díaz/CASO3/Ejercicio3.sql
+++ Sergio Díaz/CASO3/Ejercicio3.sql
@@ -1,25 +1,66 @@
 /* Alumno: Sergio Díaz */
 /* CASO 3 - Ejercicio 3: Procedimiento para calcular el total de compras por cliente y mes */
 
-CREATE PROCEDURE SDF_CASO3_RETO3 @Cliente INT
-	,@Mes VARCHAR(10) --Parametros de entrada de nuestro procedimiento. El mes en formato yyyyMM
+CREATE PROCEDURE [dbo].[SDF_CASO3_RETO3] @Cliente INT
+	,@Mes VARCHAR(10) -- Parametros de entrada del procedimiento (mes en formato YYYYMM)
 AS
-SELECT CONCAT ( -- Mensaje a mostrar de nuestro procedimiento
-		'El cliente '
-		,@Cliente
-		,' se ha gastado un total de '
-		,compras
-		,' EUR en compras de productos en el mes de '
-		,DATENAME(month, DATEFROMPARTS(LEFT(@Mes, 4), RIGHT(@Mes, 2), 1))
-		) AS MENSAJE
-FROM (
-	SELECT ISNULL(SUM(txn_amount), 0) AS compras
-	FROM case03.customer_transactions
-	WHERE customer_id = @Cliente
-		AND txn_type = 'purchase'
-		AND FORMAT(txn_date, 'yyyyMM') = @Mes
-	) COMPRAS;
+BEGIN
+	IF (
+			--Chequeamos si existe el cliente		
+			EXISTS (
+				SELECT DISTINCT customer_id
+				FROM case03.customer_transactions
+				WHERE customer_id = @Cliente
+				)
+			)
+	BEGIN
+		IF (
+				--Chequeamos si existe el mes		
+				EXISTS (
+					SELECT DISTINCT *
+					FROM case03.customer_transactions
+					WHERE FORMAT(txn_date, 'yyyyMM') = @Mes
+					)
+				)
+		BEGIN
+			SELECT CONCAT ( -- Si existe calculamos la cantidad gastada para el mes introducido
+					'El cliente '
+					,@Cliente
+					,' se ha gastado un total de '
+					,compras
+					,' EUR en compras de productos en el mes de '
+					,DATENAME(month, DATEFROMPARTS(LEFT(@Mes, 4), RIGHT(@Mes, 2), 1))
+					) AS MENSAJE
+			FROM (
+				SELECT ISNULL(SUM(txn_amount), 0) AS compras
+				FROM case03.customer_transactions
+				WHERE customer_id = @Cliente
+					AND txn_type = 'purchase'
+					AND FORMAT(txn_date, 'yyyyMM') = @Mes
+				) COMPRAS;
+		END
+		ELSE
+		BEGIN
+			SELECT CONCAT ( -- Mensaje a mostrar si no existe el mes introducido o no es correcto
+					'El mes '
+					,@Mes
+					,' no existe en la tabla o bien no es correcto (formato YYYYMM)'
+					) AS MENSAJE
+		END
+	END
+	ELSE
+	BEGIN
+		SELECT CONCAT ( -- Mensaje a mostrar si no existe el cliente en el origen de datos
+				'El cliente '
+				,@Cliente
+				,' no existe'
+				) AS MENSAJE
+	END
+END
 
 
-EXEC SDF_CASO3_RETO3 @Cliente = 1
-	,@Mes = '202003';-- Ejecutamos nuestro procedimiento para comprobar el funcionamiento
+----------Ejecuciones del procedimiento--------------
+EXEC SDF_CASO3_RETO3 306, '202003'
+EXEC SDF_CASO3_RETO3 306, '202103'
+EXEC SDF_CASO3_RETO3 308, '202001'
+EXEC SDF_CASO3_RETO3 951, '202002'
