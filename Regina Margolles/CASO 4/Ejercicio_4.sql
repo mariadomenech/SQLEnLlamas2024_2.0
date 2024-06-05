974f0e9e5fb6149a9c6c4bd021125497dcee006e
ReginaMargolles81 <140602073+ReginaMargolles81@users.noreply.github.com>
2024-06-05 15:52:55 +0200

    Create Ejercicio_4.sql

---
+++ Regina Margolles/CASO 4/Ejercicio_4.sql
@@ -0,0 +1,30 @@
+select  distinct
+		'Continuo' as Tipo_de_percentil,
+		 PERCENTILE_CONT(0.25) within group(order by benefit) over () as percentile_25,
+		 PERCENTILE_CONT(0.50) within group(order by benefit) over () as percentile_50,
+		 PERCENTILE_CONT(0.75) within group(order by benefit) over () as percentile_75 
+from(
+		select  SUM(qty * (price - (price * discount * 0.01))) as benefit,
+				    txn_id 
+		from(
+				select distinct * 
+				from case04.sales
+			) as t
+		group by txn_id
+		)as t2
+
+UNION
+
+Select 'Discreto' as Tipo_de_percentil,
+		 PERCENTILE_DISC(0.25) within group(order by benefit) over () as percentile_disc_25,
+		 PERCENTILE_DISC(0.50) within group(order by benefit) over () as percentile_disc_50,
+		 PERCENTILE_DISC(0.75) within group(order by benefit) over () as percentile_disc_75
+from(
+		select  SUM(qty * (price - (price * discount * 0.01))) as benefit,
+				    txn_id 
+		from(
+				select distinct * 
+				from case04.sales
+			) as t
+		group by txn_id
+		)as t2;
