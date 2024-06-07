7ff807de085ccb001d7841c4bd29d82600ed89d7
ReginaMargolles81 <140602073+ReginaMargolles81@users.noreply.github.com>
2024-06-07 11:14:04 +0200

    Update Ejercicio_1.sql

--- Regina Margolles/Caso 1/Ejercicio_1.sql
+++ Regina Margolles/Caso 1/Ejercicio_1.sql
@@ -29,3 +29,15 @@ GROUP BY c.customer_id;
 Por último, dale un alias a la columna isnull(SUM(m.price),0) y PERFECTO!
 
 */
+/*****************************************************
+		CORRECION 
+*****************************************************/
+Select c.customer_id, 
+	   isnull(SUM(m.price),0) 
+from 
+	  ( case01.customers as c 
+	  left join case01.sales as s
+		on c.customer_id = s.customer_id )
+	  left join case01.menu as m
+		on m.product_id = s.product_id
+group by c.customer_id;
