ae155e4c67c13e22ee3687009a2204c0e4535649
ReginaMargolles81 <140602073+ReginaMargolles81@users.noreply.github.com>
2024-06-08 18:14:13 +0200

    Update Ejercicio_3.sql

--- Regina Margolles/Caso 2/Ejercicio_3.sql
+++ Regina Margolles/Caso 2/Ejercicio_3.sql
@@ -133,3 +133,112 @@ Legibilidad: OK, pero reduciría el uso de CTEs, hay muchos cálculos que puedes
 
 
 */
+
+/*********************************************************/
+/***************** CORRECIÓN *********************/
+/*********************************************************/
+
+
+with 
+CTE_runner_orders_clean 
+as 
+	(	
+		Select
+		         order_id, 
+			 runner_id,
+			 pickup_time,
+			 CASE
+				  WHEN distance = '' THEN NULL
+				  WHEN ISNUMERIC(distance) = 1 THEN distance
+				  WHEN PATINDEX('%[A-Za-z]%',distance) = 1 THEN NULL
+				  WHEN PATINDEX('%[0-9]%',distance) = 1 THEN TRIM(SUBSTRING(distance,0, CHARINDEX('k',distance)))
+			END as distance,
+			CASE
+				  WHEN duration = '' THEN NULL
+				  WHEN ISNUMERIC(duration) = 1 THEN duration
+				  WHEN PATINDEX('%[A-Za-z]%',duration) = 1 THEN NULL
+				  WHEN PATINDEX('%[0-9]%',duration) = 1 THEN TRIM(SUBSTRING(duration,0, CHARINDEX('m',duration)))
+		    END as duration,
+			CASE
+				  WHEN cancellation = '' THEN NULL
+				  WHEN PATINDEX('%null%',cancellation) = 1 THEN NULL
+				  WHEN PATINDEX('%[A-Za-z]%',cancellation) = 1 THEN cancellation
+		    END as cancellation
+		
+	  from 
+			case02.runner_orders
+	)
+	,
+CTE_customer_orders_clean 
+as 
+	(
+		Select 
+			  order_id, 
+			  customer_id,
+			  pizza_id,
+			  CASE 
+				 WHEN PATINDEX('',exclusions)=1 THEN NULL
+				 WHEN PATINDEX('%null%',exclusions)=1 THEN NULL
+				 WHEN PATINDEX('%[0-9]%',exclusions)=1 THEN exclusions
+				 WHEN PATINDEX('%[A-Z-a-z]%',exclusions)=1 THEN (SELECT CAST(topping_id AS VARCHAR(4)) 
+							                          FROM case02.pizza_toppings 
+										  WHERE topping_name = exclusions)
+			  END as exclusions, 
+			  CASE 
+				 WHEN PATINDEX('',extras)=1 THEN NULL
+				 WHEN PATINDEX('%null%',extras)=1 THEN NULL
+				 WHEN PATINDEX('%[0-9]%',extras)=1 THEN extras
+				 WHEN PATINDEX('%[A-Z-a-z]%',extras)=1 THEN (SELECT CAST(topping_id AS VARCHAR(4)) 
+										     FROM case02.pizza_toppings 
+										     WHERE topping_name = extras)
+			  END as extras,
+			  order_time	 
+		 from
+		     case02.customer_orders
+	 ),
+
+CTE1 
+as
+(Select c.order_id,
+		c.pizza_id, 
+		p.pizza_name,
+		c.extras, 
+		r.runner_id,
+		r.distance, 
+		CASE
+	      	WHEN 
+			p.pizza_name = 'Meatlovers'  THEN 12
+	      	WHEN 
+	       		p.pizza_name = 'Vegetarian'   THEN 10
+	    	END as price_pizza,
+		(cast(r.distance as decimal(5,2)) * 0.30) as travel_cost,
+		 (SELECT count(*) FROM STRING_SPLIT(c.extras,',')) as extras_pizza
+ from CTE_customer_orders_clean as c
+     left join CTE_runner_orders_clean as r
+	on c.order_id = r.order_id
+     inner join case02.pizza_names as p
+	on c.pizza_id = p.pizza_id
+ where r.cancellation is null),
+  
+CTE2
+as(
+select CTE1.order_id,
+	   CTE1.travel_cost,
+       SUM(CTE1.price_pizza + (CTE1.extras_pizza)) over (partition by CTE1.order_id) as subtotal
+from CTE1
+),
+
+CTE3
+as(
+
+select  distinct (CTE2.order_id),
+		(CTE2.subtotal - CTE2.travel_cost) as profit
+
+from CTE2
+
+)
+select SUM(profit) as profit from CTE3;
+
+
+
+
