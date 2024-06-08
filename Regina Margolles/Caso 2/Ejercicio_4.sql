dfea9376a105727b7eabb01309e2b4731893f8e3
ReginaMargolles81 <140602073+ReginaMargolles81@users.noreply.github.com>
2024-06-08 18:17:05 +0200

    Update Ejercicio_4.sql

--- Regina Margolles/Caso 2/Ejercicio_4.sql
+++ Regina Margolles/Caso 2/Ejercicio_4.sql
@@ -32,3 +32,27 @@ Si es verdad, que en la línea 14, haríamos un RIGHT JOIN porque la tabla de to
 Puede haber más toppings en la tabla de toppings que los comentados en la tabla de recetas.
 
 */
+
+/*********************************************************/
+/***************** CORRECIÓN *********************/
+/*********************************************************/
+
+
+select  p.topping_name, COUNT (id_topping) as veces_usado
+from
+(
+		select * 
+		from case02.pizza_recipes_split
+		unpivot
+		(
+		   id_topping
+			for [topping] in ([topping_1],[topping_2],[topping_3],[topping_4],[topping_5],[topping_6],[topping_7],[topping_8])
+		) as p
+
+) as t
+
+ right join case02.pizza_toppings as p
+ 	on id_topping = p.topping_id
+
+group by id_topping, p.topping_name
+order by veces_usado desc;
