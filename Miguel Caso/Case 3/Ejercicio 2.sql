696a853b35f86e0880c33109228382868ad57887
irenelacivica <156344357+irenelacivica@users.noreply.github.com>
2024-06-12 10:39:31 +0200

    Correción Irene - Case3 Ejercicio 2.sql

--- Miguel Caso/Case 3/Ejercicio 2.sql
+++ Miguel Caso/Case 3/Ejercicio 2.sql
@@ -35,3 +35,47 @@ SELECT
 FROM selected_txn st
 WHERE (st.deposit_qty > 1 AND st.purchase_qty>1) OR st.withdrawal_qty > 1
 GROUP BY txn_year, txn_month;
+
+
+/*********************************************************/
+/***************** COMENTARIO IRENE **********************/
+/*********************************************************/
+/*
+RESULTADO: Correcto. 
+CÓDIGO: Correcto. No hubiese hecho falta poner la primera cte, podrías haberlo hecho todo en la segunda, pero si es porque querías hacerlo por pasos para tenerlo todo más claro, también vale jeje.
+Ejemplo de cómo hacerlo todo en una cte:
+	WITH selected_txn
+	AS (
+		SELECT customer_id
+			,YEAR(txn_date) AS txn_year
+			,FORMAT(txn_date, 'MMM', 'ES') AS txn_month
+			,SUM(CASE
+				WHEN txn_type = 'deposit' THEN 1
+				ELSE 0
+			END) deposit_qty
+			,SUM(CASE
+				WHEN txn_type = 'purchase' THEN 1
+				ELSE 0
+			END) purchase_qty
+			,SUM(CASE
+				WHEN txn_type = 'withdrawal' THEN 1
+				ELSE 0
+			END) withdrawal_qty
+		FROM case03.customer_transactions
+		GROUP BY customer_id
+		,YEAR(txn_date)
+		,FORMAT(txn_date, 'MMM', 'ES')
+	)
+	SELECT 
+		txn_year,
+		txn_month,
+		COUNT (customer_id) AS client_number
+	FROM selected_txn st
+	WHERE (st.deposit_qty > 1 AND st.purchase_qty>1) OR st.withdrawal_qty > 1
+	GROUP BY txn_year, txn_month;
+
+LEGIBILIDAD: Correcto.
+
+Genial Miguel!!!
+
+*/
